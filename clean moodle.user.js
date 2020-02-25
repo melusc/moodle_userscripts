47852336c6ea879538857675f2114b36f3ba922b
melusc <61409445+melusc@users.noreply.github.com>
2020-02-25 08:45:52 +0100

    Update clean moodle.user.js

--- clean moodle.user.js
+++ clean moodle.user.js
@@ -1,7 +1,7 @@
 // ===UserScript===
 // @name        Clean moodle
 // @grant       none
-// @version     3.3.2
+// @version     3.3.4
 // @include     *://moodle.ksasz.ch/*
 // @downloadURL https://github.com/melusc/lusc/raw/master/clean%20moodle.user.js
 // @updateURL   https://github.com/melusc/lusc/raw/master/clean%20moodle.user.js
@@ -11,7 +11,6 @@
 
 //This is for moodle.ksasz.ch and it was written for me, so if you install it it might not work perfectly for you.
 //To fix that you can modify the code for yourself, it's pretty straightforward to do.
-
 
 //true is active, false isn't
 
@@ -106,171 +105,169 @@ var customReplaceWith3 = 'placeholder'; //Replace with
 
 //Code
 
-window.onload = function fun() {
 
-    //Herbstwoche
-    var id1 = 'label_3_8';
-    if (hw === true) {
-        var elem1 = document.getElementById(id1);
-        elem1.parentNode.removeChild(elem1);
+
+//Herbstwoche
+var id1 = 'label_3_8';
+if (hw === true) {
+    var elem1 = document.getElementById(id1);
+    elem1.parentNode.removeChild(elem1);
+}
+
+//ECDL
+var id2 = 'label_3_7';
+if (ec === true) {
+    var elem2 = document.getElementById(id2);
+    elem2.parentNode.removeChild(elem2);
+}
+
+//Studienwahlparcours
+var id3 = 'label_3_9';
+if (sw === true) {
+    var elem345 = document.getElementById(id3);
+    elem345.parentNode.removeChild(elem345);
+}
+
+//Umfrage S2d
+var id4 = 'label_3_10';
+if (uf === true) {
+    var elem45 = document.getElementById(id4);
+    elem45.parentNode.removeChild(elem45);
+}
+
+//Weihnachtslauf
+var id6 = 'label_3_12';
+if (wl === true) {
+    var elem6 = document.getElementById(id6);
+    elem6.parentNode.removeChild(elem6);
+}
+
+//Febwoche einschreiben
+var id7 = 'label_3_13';
+if (fs === true) {
+    var elem7 = document.getElementById(id7);
+    elem7.parentNode.removeChild(elem7);
+}
+
+//Schneesportlager
+var id8 = 'label_3_14';
+if (sl === true) {
+    var elem8 = document.getElementById(id8);
+    elem8.parentNode.removeChild(elem8);
+}
+
+//Geschichte HuI
+var id9 = 'label_3_16';
+if (gs === true) {
+    document.getElementById(id9).innerHTML = document.getElementById(id9).innerHTML.replace('Geschichte HuI', rgs);
+}
+
+//SPFWahl
+var id10 = 'label_3_6';
+if (sp === true) {
+    var elem10 = document.getElementById(id10);
+    elem10.parentNode.removeChild(elem10);
+} else {
+    document.getElementById(id10).innerHTML = document.getElementById(id10).innerHTML.replace('Wahl des Schwerpunktfaches für 1. Gymi-Klassen 2020', rsp);
+}
+
+//Klassenstunde
+var id11 = 'label_3_15';
+if (kl === true) {
+    document.getElementById(id11).innerHTML = document.getElementById(id11).innerHTML.replace('Klassenstunde UmA 2019-2023 - M1a', rkl);
+}
+
+//Geografie
+var id12 = 'label_3_17';
+if (gg === true) {
+    document.getElementById(id12).innerHTML = document.getElementById(id12).innerHTML.replace('Geografie DiR FM1\/M1a 19-20', rgg);
+}
+
+//Physik
+var id13 = 'label_3_18';
+if (ps === true) {
+    document.getElementById(id13).innerHTML = document.getElementById(id13).innerHTML.replace('Physik BiM M1a', rps);
+}
+
+//Mathematik
+var id14 = 'label_3_19';
+if (ma === true) {
+    document.getElementById(id14).innerHTML = document.getElementById(id14).innerHTML.replace('Mathematik UmA 2019-2020 - M1a', rma);
+}
+
+//Wirtschaft
+var id15 = 'label_3_20';
+if (wr === true) {
+    document.getElementById(id15).innerHTML = document.getElementById(id15).innerHTML.replace('Wirtschaft und Recht M1a DiD', rwr);
+}
+
+//Deutsch
+var id16 = 'label_3_21';
+if (de === true) {
+    document.getElementById(id16).innerHTML = document.getElementById(id16).innerHTML.replace('Deutsch HaK M1a 2019\/20', rde);
+}
+
+//Biologie
+var id17 = 'label_3_22';
+if (bi === true) {
+    document.getElementById(id17).innerHTML = document.getElementById(id17).innerHTML.replace('Biologie WiD M1a 19\/20', rbi);
+}
+
+//Französisch
+var id18 = 'label_3_23';
+if (fr === true) {
+    document.getElementById(id18).innerHTML = document.getElementById(id18).innerHTML.replace('Französisch M1a FeM SJ 19\/20', rfr);
+}
+
+//Geschichte M1a
+var id19 = 'label_3_24';
+if (gr === true) {
+    var elem19 = document.getElementById(id19);
+    elem19.parentNode.removeChild(elem19);
+} else {
+    if (gm === true) {
+        document.getElementById(id19).innerHTML = document.getElementById(id19).innerHTML.replace('Geschichte M1a', rgm);
     }
+}
 
-    //ECDL
-    var id2 = 'label_3_7';
-    if (ec === true) {
-        var elem2 = document.getElementById(id2);
-        elem2.parentNode.removeChild(elem2);
-    }
+//Chemie
+var id20 = 'label_3_25';
+if (ch === true) {
+    document.getElementById(id20).innerHTML = document.getElementById(id20).innerHTML.replace('Chemie WyE M1a', rch);
+}
 
-    //Studienwahlparcours
-    var id3 = 'label_3_9';
-    if (sw === true) {
-        var elem345 = document.getElementById(id3);
-        elem345.parentNode.removeChild(elem345);
-    }
+//Musik
+var id21 = 'label_3_26';
+if (mu === true) {
+    document.getElementById(id21).innerHTML = document.getElementById(id21).innerHTML.replace('Musik AlC Grundlagenfach', rmu);
+}
 
-    //Umfrage S2d
-    var id4 = 'label_3_10';
-    if (uf === true) {
-        var elem45 = document.getElementById(id4);
-        elem45.parentNode.removeChild(elem45);
-    }
-
-    //Weihnachtslauf
-    var id6 = 'label_3_12';
-    if (wl === true) {
-        var elem6 = document.getElementById(id6);
-        elem6.parentNode.removeChild(elem6);
-    }
-
-    //Febwoche einschreiben
-    var id7 = 'label_3_13';
-    if (fs === true) {
-        var elem7 = document.getElementById(id7);
-        elem7.parentNode.removeChild(elem7);
-    }
-
-    //Schneesportlager
-    var id8 = 'label_3_14';
-    if (sl === true) {
-        var elem8 = document.getElementById(id8);
-        elem8.parentNode.removeChild(elem8);
-    }
-
-    //Geschichte HuI
-    var id9 = 'label_3_16';
-    if (gs === true) {
-        document.getElementById(id9).innerHTML = document.getElementById(id9).innerHTML.replace('Geschichte HuI', rgs);
-    }
-
-    //SPFWahl
-    var id10 = 'label_3_6';
-    if (sp === true) {
-        var elem10 = document.getElementById(id10);
-        elem10.parentNode.removeChild(elem10);
+//Custom1
+if (customActive1 === true){
+    if (custom1 === false){
+        var elem22 = document.getElementById(customId1);
+        elem22.parentNode.removeChild(elem22);
     } else {
-        document.getElementById(id10).innerHTML = document.getElementById(id10).innerHTML.replace('Wahl des Schwerpunktfaches für 1. Gymi-Klassen 2020', rsp);
+        document.getElementById(customId1).innerHTML = document.getElementById(customId1).innerHTML.replace(customReplace1, customReplaceWith1);
     }
+}
 
-    //Klassenstunde
-    var id11 = 'label_3_15';
-    if (kl === true) {
-        document.getElementById(id11).innerHTML = document.getElementById(id11).innerHTML.replace('Klassenstunde UmA 2019-2023 - M1a', rkl);
-    }
-
-    //Geografie
-    var id12 = 'label_3_17';
-    if (gg === true) {
-        document.getElementById(id12).innerHTML = document.getElementById(id12).innerHTML.replace('Geografie DiR FM1\/M1a 19-20', rgg);
-    }
-
-    //Physik
-    var id13 = 'label_3_18';
-    if (ps === true) {
-        document.getElementById(id13).innerHTML = document.getElementById(id13).innerHTML.replace('Physik BiM M1a', rps);
-    }
-
-    //Mathematik
-    var id14 = 'label_3_19';
-    if (ma === true) {
-        document.getElementById(id14).innerHTML = document.getElementById(id14).innerHTML.replace('Mathematik UmA 2019-2020 - M1a', rma);
-    }
-
-    //Wirtschaft
-    var id15 = 'label_3_20';
-    if (wr === true) {
-        document.getElementById(id15).innerHTML = document.getElementById(id15).innerHTML.replace('Wirtschaft und Recht M1a DiD', rwr);
-    }
-
-    //Deutsch
-    var id16 = 'label_3_21';
-    if (de === true) {
-        document.getElementById(id16).innerHTML = document.getElementById(id16).innerHTML.replace('Deutsch HaK M1a 2019\/20', rde);
-    }
-
-    //Biologie
-    var id17 = 'label_3_22';
-    if (bi === true) {
-        document.getElementById(id17).innerHTML = document.getElementById(id17).innerHTML.replace('Biologie WiD M1a 19\/20', rbi);
-    }
-
-    //Französisch
-    var id18 = 'label_3_23';
-    if (fr === true) {
-        document.getElementById(id18).innerHTML = document.getElementById(id18).innerHTML.replace('Französisch M1a FeM SJ 19\/20', rfr);
-    }
-
-    //Geschichte M1a
-    var id19 = 'label_3_24';
-    if (gr === true) {
-        var elem19 = document.getElementById(id19);
-        elem19.parentNode.removeChild(elem19);
+//Custom2
+if (customActive2 === true){
+    if (custom2 === false){
+        var elem23 = document.getElementById(customId2);
+        elem23.parentNode.removeChild(elem23);
     } else {
-        if (gm === true) {
-            document.getElementById(id19).innerHTML = document.getElementById(id19).innerHTML.replace('Geschichte M1a', rgm);
-        }
+        document.getElementById(customId2).innerHTML = document.getElementById(customId2).innerHTML.replace(customReplace2, customReplaceWith2);
     }
+}
 
-    //Chemie
-    var id20 = 'label_3_25';
-    if (ch === true) {
-        document.getElementById(id20).innerHTML = document.getElementById(id20).innerHTML.replace('Chemie WyE M1a', rch);
+//Custom3
+if (customActive3 === true){
+    if (custom3 === false){
+        var elem24 = document.getElementById(customId3);
+        elem24.parentNode.removeChild(elem24);
+    } else {
+        document.getElementById(customId3).innerHTML = document.getElementById(customId3).innerHTML.replace(customReplace3, customReplaceWith3);
     }
-
-    //Musik
-    var id21 = 'label_3_26';
-    if (mu === true) {
-        document.getElementById(id21).innerHTML = document.getElementById(id21).innerHTML.replace('Musik AlC Grundlagenfach', rmu);
-    }
-
-    //Custom1
-    if (customActive1 === true){
-        if (custom1 === false){
-            var elem22 = document.getElementById(customId1);
-            elem22.parentNode.removeChild(elem22);
-        } else {
-            document.getElementById(customId1).innerHTML = document.getElementById(customId1).innerHTML.replace(customReplace1, customReplaceWith1);
-        }
-    }
-
-    //Custom2
-    if (customActive2 === true){
-        if (custom2 === false){
-            var elem23 = document.getElementById(customId2);
-            elem23.parentNode.removeChild(elem23);
-        } else {
-            document.getElementById(customId2).innerHTML = document.getElementById(customId2).innerHTML.replace(customReplace2, customReplaceWith2);
-        }
-    }
-
-
-    //Custom3
-    if (customActive3 === true){
-        if (custom3 === false){
-            var elem24 = document.getElementById(customId3);
-            elem24.parentNode.removeChild(elem24);
-        } else {
-            document.getElementById(customId3).innerHTML = document.getElementById(customId3).innerHTML.replace(customReplace3, customReplaceWith3);
-        }
-    }
-};
+}
