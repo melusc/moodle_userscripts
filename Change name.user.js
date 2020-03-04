09e155eaea6b53a3207fc82b9c77749ed62cfca6
melusc <61409445+melusc@users.noreply.github.com>
2020-03-04 18:14:49 +0100

    Update Change name.user.js

--- Change name.user.js
+++ Change name.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name         .
-// @namespace    .
-// @version      1.4.1
+// @name         Alberto Müller
+// @version      1.5
 // @author       lusc
-// @match        moodle.ksasz.ch/*
+// @match        https://moodle.ksasz.ch/user/profile.php?*
 // @grant        none
 // ==/UserScript==
 
-document.querySelector('body').innerHTML = document.querySelector('body').innerHTML.replace(/Caprioni/g,'Müller');
-document.querySelector('body').innerHTML = document.querySelector('body').innerHTML.replace(/caprioni/g,'müller');
+let script = document.createElement('script');
+script.innerHTML = `window.onload=function fun(){let input=document.createElement('input');input.setAttribute('type','button');input.setAttribute('value','Beautify profile');input.setAttribute('class','buttons');input.addEventListener('click',fun1,false);if(document.querySelector('#exploreProfiles')){document.querySelector('.page-header-headings').insertBefore(input,document.querySelector('.page-header-headings').children[2])}else{console.log(false)}function fun1(){document.querySelector('body').innerHTML=document.querySelector('body').innerHTML.replace(/Caprioni/g,'Müller');document.querySelector('body').innerHTML=document.querySelector('body').innerHTML.replace(/caprioni/g,'müller')}};`;
+document.head.appendChild(script);
