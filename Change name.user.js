feddadebda283e4540002ff719469fd83446eff5
melusc <61409445+melusc@users.noreply.github.com>
2020-03-05 11:31:33 +0100

    Update Change name.user.js

--- Change name.user.js
+++ Change name.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Alberto Müller
-// @version      1.5.2
+// @version      1.5.3
 // @author       lusc
 // @match        https://moodle.ksasz.ch/user/profile.php?*
 // @grant        none
@@ -9,5 +9,54 @@
 // ==/UserScript==
 
 let script = document.createElement('script');
-script.innerHTML = `if(document.querySelector('.page-header-headings').children[0].innerHTML=='Alberto Caprioni'){let whitespace=document.createElement('span'),input=document.createElement('input');whitespace.innerHTML='&nbsp;&nbsp;&nbsp;';whitespace.setAttribute('style','display:inline');whitespace.id='whitespace';input.setAttribute('type','button');input.setAttribute('value','Beautify profile');input.setAttribute('class','buttons');input.setAttribute('id','beautifyProfile');input.setAttribute('onclick',"document.querySelector('body').innerHTML=document.querySelector('body').innerHTML.replace(/Caprioni/g,'Müller');document.querySelector('body').innerHTML=document.querySelector('body').innerHTML.replace(/caprioni/g,'müller');let thisHeading = document.querySelector('#beautifyProfile'); thisHeading.parentNode.removeChild(thisHeading);let whitespace = document.querySelector('#whitespace'); whitespace.parentNode.removeChild(whitespace)");if(document.querySelector('#exploreProfiles')){document.querySelector('.page-header-headings').appendChild(input);document.querySelector('.page-header-headings').insertBefore(whitespace,document.querySelector('.page-header-headings').children[3])}else{document.querySelector('.page-header-headings').children[0].setAttribute('style','display:inline');input.setAttribute('style','display:inline');document.querySelector('.page-header-headings').appendChild(whitespace);document.querySelector('.page-header-headings').appendChild(input)}}`;
+script.innerHTML = `if (document.querySelector('.page-header-headings').children[0].innerHTML == 'Alberto Caprioni') {
+    let whitespace = document.createElement('span'),
+        input = document.createElement('input');
+
+    whitespace.innerHTML = '&nbsp;&nbsp;&nbsp;';
+    whitespace.setAttribute('style', 'display:inline');
+    whitespace.id = 'whitespace';
+
+    input.setAttribute('type', 'button');
+    input.setAttribute('value', 'Beautify profile');
+    input.setAttribute('class', 'buttons');
+    input.setAttribute('id', 'profileChanger');
+    input.setAttribute('onclick', 'beautify()');
+
+    if (document.querySelector('#exploreProfiles')) {
+        document
+            .querySelector('.page-header-headings')
+            .appendChild(input);
+        document
+            .querySelector('.page-header-headings')
+            .insertBefore(whitespace, document.querySelector('.page-header-headings').children[3]);
+    } else {
+        document
+            .querySelector('.page-header-headings')
+            .children[0]
+            .setAttribute('style', 'display:inline');
+        input.setAttribute('style', 'display:inline');
+        document
+            .querySelector('.page-header-headings')
+            .appendChild(whitespace);
+        document
+            .querySelector('.page-header-headings')
+            .appendChild(input);
+    }
+}
+
+function beautify() {
+    document.querySelector('body').innerHTML = document.querySelector('body').innerHTML.replace(/Caprioni/g, 'Müller');
+    document.querySelector('body').innerHTML = document.querySelector('body').innerHTML.replace(/caprioni/g, 'müller');
+
+    document.querySelector('#profileChanger').value = 'Uglify Profile';
+    document.querySelector('#profileChanger').setAttribute('onclick','uglify()');
+}
+function uglify() {
+    document.querySelector('body').innerHTML = document.querySelector('body').innerHTML.replace(/Müller/g, 'Caprioni');
+    document.querySelector('body').innerHTML = document.querySelector('body').innerHTML.replace(/müller/g, 'caprioni');
+
+    document.querySelector('#profileChanger').value = 'Beautify Profile';
+    document.querySelector('#profileChanger').setAttribute('onclick','beautify()');
+}`;
 document.head.appendChild(script);
