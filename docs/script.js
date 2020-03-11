833353ef11af3c72e55af19cccda2c97f2e71cf7
melusc <61409445+melusc@users.noreply.github.com>
2020-03-11 19:59:27 +0100

    :fire:

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,4 @@
-function getCokie(name) {
+function getCookie(name) {
 	let value = "; " + document.cookie;
 	let parts = value.split("; " + name + "=");
 	if (parts.length == 2) return parts.pop().split(";").shift();
