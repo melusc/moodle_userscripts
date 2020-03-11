4f62e8167b14173fdb8bc227bfd162e6b15a30a9
melusc <61409445+melusc@users.noreply.github.com>
2020-03-11 16:51:02 +0100

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,20 +1,55 @@
+function getCookie(name) {
+  let value = "; " + document.cookie;
+  let parts = value.split("; " + name + "=");
+  if (parts.length == 2) return parts.pop().split(";").shift();
+}
 let boom = 'Boom!',
-    num = 1;
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+	num = 1,
+	record = getCookie('record');
+	
+if (!record){
+	record = 1;
+}
+if (record > 1) {
+	document.querySelector('h2.record').innerHTML = `Current record: ${record}`;
+}
+console.log(record);
+function run(boom) {
+	num++;
+	if (boom) {
+		if (Math.floor((num - 1) / 7) == (num - 1) / 7 || Math.floor((num - 1) / 3) == (num - 1) / 3) {
+			document.querySelector('h1.number').innerHTML = num;
+		} else {
+			failed();
+		}
+	} else {
+		if (!(Math.floor((num - 1) / 7) == (num - 1) / 7 || Math.floor((num - 1) / 3) == (num - 1) / 3)) {
+			document.querySelector('h1.number').innerHTML = num;
+		} else {
+			failed();
+		}
+	}
 }
 
-function run() {
-    num++;
-    console.log(`By 7: ${Math.floor(num / 7) == num / 7}`);
-    console.log(`By 3: ${Math.floor(num / 3) == num / 3}`);
-    console.log(`Num: ${num}`);
-    if (Math.floor(num / 7) == num / 7) {
-        document.querySelector('h1.number').innerHTML = boom;
-    } else if (Math.floor(num / 3) == num / 3) {
-        document.querySelector('h1.number').innerHTML = boom;
-    } else {
-        document.querySelector('h1.number').innerHTML = num;
-    }
-    document.querySelector('h2.number').innerHTML = num;
+function failed() {
+	document.querySelector('button.nothing').setAttribute('onclick', 'reset()');
+	document.querySelector('button.nothing').innerHTML = 'Reset?';
+	document.querySelector('button.boom').style.display = 'none';
+	document.body.style.backgroundColor = 'red';
+	document.querySelector('h3.text').style.color = 'white';
+	if (record < num - 1) {
+		record = num - 1;
+		document.querySelector('h2.record').innerHTML = `Current record: ${record}`;
+		document.cookie = `record=${record}`
+	}
+}
+
+function reset() {
+	document.querySelector('button.nothing').setAttribute('onclick', 'run()');
+	document.querySelector('button.nothing').innerHTML = 'Safe';
+	document.querySelector('button.boom').style.display = '';
+	document.body.style.backgroundColor = '';
+	document.querySelector('h3.text').style.color = '';
+	num = 1
+	document.querySelector('h1.number').innerHTML = num;
 }
