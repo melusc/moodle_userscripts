b5dff88b0788577bf01339fbbedd013e9d331713
melusc <61409445+melusc@users.noreply.github.com>
2020-03-11 19:59:14 +0100

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -1,4 +1,4 @@
-function getCookie(name) {
+function getCokie(name) {
 	let value = "; " + document.cookie;
 	let parts = value.split("; " + name + "=");
 	if (parts.length == 2) return parts.pop().split(";").shift();
