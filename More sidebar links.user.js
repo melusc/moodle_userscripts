24d126cd7e114cd828be6869e1b4aea0886cc5e8
melusc <61409445+melusc@users.noreply.github.com>
2020-03-22 15:31:45 +0100

    :fire:
    Now possible to add custom searchbars

--- More sidebar links.user.js
+++ More sidebar links.user.js
@@ -1,13 +1,16 @@
 // ==UserScript==
 // @name         More sidebar links
-// @version      1.4
+// @description  Adds more links to the sidebar in moodle and is fully customisable
+// @version      1.5
 // @author       lusc
 // @match        https://moodle.ksasz.ch/*
+// @exclude      *://moodle.ksasz.ch/online*
 // @downloadURL  https://github.com/melusc/lusc/raw/master/More%20sidebar%20links.user.js
 // @updateURL    https://github.com/melusc/lusc/raw/master/More%20sidebar%20links.user.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js#sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=
 // ==/UserScript==
-
-//Currently adds schulNetz, current lunch menu, KSA printing website
+'use strict';
+//Currently adds schulNetz, current lunch menu, KSA printing website and a duden search bar
 
 
 //Code
@@ -33,26 +36,28 @@ if (result.length == 1) {
 }
 
 const linkMensa = `https://www.ksasz.ch/images/PDF-Dokumente/Mensa/MeWo${result}.pdf`;
+let doc = document.querySelector('[class="type_system depth_2 contains_branch"] > ul');
 
-run('Mensa Menü',linkMensa);
-run('schulNetz','https://www.schul-netz.com/ausserschwyz/');
-run('Drucker','http://printer.ksasz.ch:9191/');
+add('Drucker', 'http://printer.ksasz.ch:9191/');
+add('Mensa Menü', linkMensa);
+add('schulNetz', 'https://www.schul-netz.com/ausserschwyz/');
+search('https://www.duden.de/suchen/dudenonline/%s','duden.de');
+search('https://en.wikipedia.org/w/index.php?search=%s','en.wikipedia.org');
 
 
-function run(name, link) {
+function add(name, link) {
     let li = document.createElement('li'),
         p = document.createElement('p'),
         a = document.createElement('a'),
         img = document.createElement('img'),
-        span = document.createElement('span'),
-        doc = document.querySelector('[class="type_system depth_2 contains_branch"]').children[1];
+        span = document.createElement('span');
 
     img.setAttribute('class', 'icon navicon');
-    img.setAttribute('aria-hidden',true);
+    img.setAttribute('aria-hidden', true);
     img.setAttribute('src', 'https://moodle.ksasz.ch/theme/image.php/clean/core/1557402677/i/course');
-    img.setAttribute('tabindex',-1);
+    img.setAttribute('tabindex', -1);
 
-    a.setAttribute('tabindex',-1);
+    a.setAttribute('tabindex', -1);
     a.id = `label_${name.toLowerCase().replace(' ','_')}`;
     a.setAttribute('title', name);
     a.setAttribute('href', link);
@@ -60,10 +65,10 @@ function run(name, link) {
     a.setAttribute('rel', 'noopener noreferrer');
 
     p.setAttribute('class', 'tree_item hasicon');
-    p.setAttribute('role','treeitem');
+    p.setAttribute('role', 'treeitem');
     p.id = `expandable_branch_${name.toLowerCase().replace(' ','_')}`;
 
-    span.setAttribute('class','item-content-wrap');
+    span.setAttribute('class', 'item-content-wrap');
     span.appendChild(document.createTextNode(name));
 
     a.appendChild(img);
@@ -74,4 +79,21 @@ function run(name, link) {
     li.appendChild(p);
 
     doc.appendChild(li);
+}
+
+function search(url,name){
+    let input = document.createElement('input'),
+        sanitisedName = name.toLowerCase().replace(/[^a-z0-9]/g,'');
+    name = name.replace(/[^a-zA-Z0-9:\/.&;]/g,'');
+    input.placeholder = `Suche auf ${name}`;
+    input.type = 'text';
+    input.id = `${sanitisedName}Suche`;
+    doc.appendChild(input);
+
+    $(`#${sanitisedName}Suche`).on('keydown keyup', function(e) {
+        if (e.which === 13) {
+            window.open(url.replace('%s',e.target.value), '_blank');
+            e.target.value = '';
+        }
+    });
 }
