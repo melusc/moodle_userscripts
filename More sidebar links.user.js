e48f39552cc0906ccd5e795ded04b40387cc95f6
melusc <61409445+melusc@users.noreply.github.com>
2020-03-22 17:42:18 +0100

    :fire:

--- More sidebar links.user.js
+++ More sidebar links.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         More sidebar links
 // @description  Adds more links to the sidebar in moodle and is fully customisable
-// @version      1.5
+// @version      2020.03.22
 // @author       lusc
 // @match        https://moodle.ksasz.ch/*
 // @exclude      *://moodle.ksasz.ch/online*
@@ -10,40 +10,28 @@
 // @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js#sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=
 // ==/UserScript==
 'use strict';
-//Currently adds schulNetz, current lunch menu, KSA printing website and a duden search bar
 
-
-//Code
+//Adds schulNetz, current lunch menu, KSA printing website, a duden search bar, a wikipedia search bar and a search bar to find lowest common multiple
 
 function getWeekNumber(d) {
-    // Copy date so don't modify original
     d = new Date(Date.UTC(d.getFullYear(), d.getMonth(), d.getDate()));
-    // Set to nearest Thursday: current date + 4 - current day number
-    // Make Sunday's day number 7
     d.setUTCDate(d.getUTCDate() + 4 - (d.getUTCDay() || 7));
-    // Get first day of year
     var yearStart = new Date(Date.UTC(d.getUTCFullYear(), 0, 1));
-    // Calculate full weeks to nearest Thursday
     var weekNo = Math.ceil((((d - yearStart) / 86400000) + 1) / 7);
-    // Return array of year and week number
     return [d.getUTCFullYear(), weekNo];
 }
-
-let result = getWeekNumber(new Date());
-result = result[1];
+let result = getWeekNumber(new Date())[1];
 if (result.length == 1) {
-    result = `0${result}`;
+    result = `0${ result }`;
 }
+const linkMensa = `https://www.ksasz.ch/images/PDF-Dokumente/Mensa/MeWo${ result }.pdf`;
 
-const linkMensa = `https://www.ksasz.ch/images/PDF-Dokumente/Mensa/MeWo${result}.pdf`;
-let doc = document.querySelector('[class="type_system depth_2 contains_branch"] > ul');
-
-add('Drucker', 'http://printer.ksasz.ch:9191/');
-add('Mensa Menü', linkMensa);
-add('schulNetz', 'https://www.schul-netz.com/ausserschwyz/');
-search('https://www.duden.de/suchen/dudenonline/%s','duden.de');
-search('https://en.wikipedia.org/w/index.php?search=%s','en.wikipedia.org');
-
+add('Drucker', 'http://printer.ksasz.ch:9191/'); //Adds link to ksa printer
+add('Mensa Menü', linkMensa); //Adds link to ksa menu
+add('schulNetz', 'https://www.schul-netz.com/ausserschwyz/'); //Adds link to ksa schulNetz
+search('https://www.duden.de/suchen/dudenonline/%s', 'duden.de'); //Adds search bar for duden
+search('https://en.wikipedia.org/w/index.php?search=%s', 'en.wikipedia.org'); //Adds search bar for wikipedia
+lcm(); //Adds input to find lowest common multiple
 
 function add(name, link) {
     let li = document.createElement('li'),
@@ -51,49 +39,79 @@ function add(name, link) {
         a = document.createElement('a'),
         img = document.createElement('img'),
         span = document.createElement('span');
-
     img.setAttribute('class', 'icon navicon');
     img.setAttribute('aria-hidden', true);
     img.setAttribute('src', 'https://moodle.ksasz.ch/theme/image.php/clean/core/1557402677/i/course');
     img.setAttribute('tabindex', -1);
-
     a.setAttribute('tabindex', -1);
-    a.id = `label_${name.toLowerCase().replace(' ','_')}`;
+    a.id = `label_${name.toLowerCase().replace(/\s/g,'_')}`;
     a.setAttribute('title', name);
     a.setAttribute('href', link);
     a.setAttribute('target', '_blank');
     a.setAttribute('rel', 'noopener noreferrer');
-
     p.setAttribute('class', 'tree_item hasicon');
     p.setAttribute('role', 'treeitem');
-    p.id = `expandable_branch_${name.toLowerCase().replace(' ','_')}`;
-
+    p.id = `expandable_branch_${name.toLowerCase().replace(/\s/g,'_')}`;
     span.setAttribute('class', 'item-content-wrap');
     span.appendChild(document.createTextNode(name));
-
     a.appendChild(img);
     a.appendChild(span);
-
     p.appendChild(a);
-
     li.appendChild(p);
-
+    let doc = document.querySelector('[class="type_system depth_2 contains_branch"] > ul');
     doc.appendChild(li);
 }
 
-function search(url,name){
+function search(url, name) {
     let input = document.createElement('input'),
-        sanitisedName = name.toLowerCase().replace(/[^a-z0-9]/g,'');
-    name = name.replace(/[^a-zA-Z0-9:\/.&;]/g,'');
-    input.placeholder = `Suche auf ${name}`;
+        sanitisedName = name.toLowerCase().replace(/[^a-z0-9]/g, '');
+    name = name.replace(/[^a-zA-Z0-9:\/.&;]/g, '');
+    input.placeholder = `Suche auf ${ name }`;
     input.type = 'text';
-    input.id = `${sanitisedName}Suche`;
+    input.id = `${ sanitisedName }Suche`;
+    let doc = document.querySelector('[class="type_system depth_2 contains_branch"] > ul');
     doc.appendChild(input);
-
-    $(`#${sanitisedName}Suche`).on('keydown keyup', function(e) {
+    $(`#${ sanitisedName }Suche`).on('keydown keyup', function(e) {
         if (e.which === 13) {
-            window.open(url.replace('%s',e.target.value), '_blank');
+            window.open(url.replace('%s', e.target.value), '_blank');
             e.target.value = '';
+        }
+    });
+}
+
+function lcm() {
+    let input = document.createElement('input'),
+        url = 'https://www.calculatorsoup.com/calculators/math/lcm.php?input=%s&data=none&action=solve';
+    input.placeholder = 'Get LCM of numbers';
+    input.type = 'text';
+    input.id = 'calculatorSoupSuche';
+    let doc = document.querySelector('[class="type_system depth_2 contains_branch"] > ul');
+    doc.appendChild(input);
+    $('#calculatorSoupSuche').on('keydown keyup', function(e) {
+        if (e.which === 13) {
+            let value = e.target.value;
+            value = value.replace(/[^0-9,-.\s]+/g,'');
+            value = value.replace(/\.+/g, ',');
+            value = value.replace(/\s+/g, ',');
+            value = value.replace(/,{2,}/g, ',');
+            value = value.replace(/,$/, '');
+            value = value.replace(/^,/, '');
+            let occurences = value.split('-').length - 1;
+            if (value.includes('-') && occurences == 1) {
+                let first = Number(value.replace(/-[0-9]{1,}/, '').replace(/[^0-9]/g, '')),
+                    last = Number(value.replace(/[0-9]{1,}-/, '').replace(/[^0-9]/g, ''));
+                value = first;
+                for (let i = first + 1; i <= last; i += 1) {
+                    value += `,${ i }`;
+                }
+            }
+            value = String(value);
+            if (occurences <= 1 && value.length > 1 && value.includes(',')) {
+                window.open(url.replace('%s', value.replace(/,/g, '%2C')), '_blank');
+                e.target.value = '';
+            } else {
+                alert('Error, falsy input');
+            }
         }
     });
 }
