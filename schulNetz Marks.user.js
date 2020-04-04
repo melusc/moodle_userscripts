48b7af12d7d8c3bc035eb874f727b87a52dfa529
melusc <61409445+melusc@users.noreply.github.com>
2020-04-04 10:27:29 +0200

    :fire:

--- schulNetz Marks.user.js
+++ schulNetz Marks.user.js
@@ -1,11 +1,11 @@
 // ==UserScript==
-// @name         schulNetz Marks copy pasteable
-// @version      2020.04.04a
-// @author       lusc
-// @match        https://www.schul-netz.com/ausserschwyz/index.php?pageid=21311*
-// @downloadURL  https://github.com/melusc/lusc/raw/master/schulNetz%20Marks.user.js
-// @updateURL    https://github.com/melusc/lusc/raw/master/schulNetz%20Marks.user.js
-// @grant        none
+// @name          schulNetz Marks copy pasteable
+// @version       2020.04.04b
+// @author        lusc
+// @match         https://www.schul-netz.com/ausserschwyz/index.php?pageid=21311*
+// @downloadURL   https://github.com/melusc/lusc/raw/master/schulNetz%20Marks.user.js
+// @updateURL     https://github.com/melusc/lusc/raw/master/schulNetz%20Marks.user.js
+// @grant         none
 // ==/UserScript==
 'use strict';
 let text = {
@@ -31,58 +31,85 @@ button.style.outline = 'none';
 button.className = 'userScriptGenerated';
 
 let page = document.getElementsByTagName('page');
-Array.from(page).forEach(a => {
-    if (a.innerHTML.indexOf('Aktuelle Noten') != -1) page = a;
+Array.from(page).forEach(e => {
+    if (e.innerHTML.indexOf('Aktuelle Noten') != -1) page = e;
 });
 let h3 = page.getElementsByTagName('h3');
-Array.from(h3).forEach(a => {
-    if (a.innerHTML.indexOf('Aktuelle Noten') != -1) h3 = a;
+Array.from(h3).forEach(e => {
+    if (e.innerHTML.indexOf('Aktuelle Noten') != -1) h3 = e;
 });
 let pageChildren = Array.from(page.children),
     indexH3 = -1;
-for (let h = 0; h < pageChildren.length; h++) {
-    if (pageChildren[h] == h3) indexH3 = h;
-}
+pageChildren.forEach((e, i) => {
+    if (e == h3) indexH3 = i;
+});
 page.insertBefore(button, pageChildren[indexH3 + 1]);
 
 let style = document.createElement('style');
 style.innerHTML = '.userScriptGenerated:hover{filter: brightness(85%);}';
 document.head.appendChild(style);
 
+//Round Numbers
 const roundNum = (num, power) => {
         return Math.round(+num * Math.pow(10, +power)) / Math.pow(10, +power);
     },
+    //Create linebreak
     br = () => {
         return document.createElement('br');
     },
+    //create divider
     cDiv = () => {
-        return document.createElement('div');
+        let e = document.createElement('div');
+        e.style.display = 'inline';
+        e.style.fontSize = 'inherit';
+        return e;
     },
+    //create h3 element
     cH3 = () => {
         return document.createElement('h3');
     },
-    styler = e => {
-        e.style.display = 'inline';
-        e.style.fontSize = 'inherit';
-    },
+    //Style element red
     red = e => {
         e.style.color = 'red';
     },
+    //Style element green
     green = e => {
         e.style.color = 'green';
+    },
+    //Style textArea
+    textStyler = e => {
+        e.setAttribute('readonly', '');
+        e.setAttribute('onClick', 'this.focus();this.select()');
+        e.style.width = '300px';
+        e.style.outline = 'none';
+        e.style.height = e.scrollHeight + 'px';
+    },
+    //Inserting elements in right order
+    insert = arr => {
+        window.elRemove = arr;
+        arr.forEach(e => {
+            page.insertBefore(e, pageChildren[indexH3 + 1]);
+        });
     };
+
+
+
 window.grabMarks = () => {
     let marksTbody = document.getElementsByClassName('mdl-data-table mdl-js-data-table mdl-table--listtable');
     Array.from(marksTbody).forEach(a => {
-        if (a.innerHTML.indexOf('Kurs') != -1) marksTbody = a;
+        if (a.innerHTML.indexOf('Kurs') != -1) marksTbody = a.getElementsByTagName('tbody')[0];
     });
-    marksTbody = marksTbody.getElementsByTagName('tbody')[0];
-    let tr = Array.from(marksTbody.children),
-        trArr = [],
+
+    //Finding Marks and stringifying them
+    let textArea = document.createElement('textarea'),
+        all = cH3(),
         marksArrNum = [],
         marksArrName = [],
-        failedNums = [],
-        failedNames = [];
+        trArr = [],
+        tr = Array.from(marksTbody.children),
+        marksString = '';
+    all.innerHTML = text.all;
+    all.style.marginBottom = '6px';
     tr.forEach(a => {
         if (!a.style.display) trArr.push(a);
     });
@@ -92,102 +119,89 @@ window.grabMarks = () => {
             marksArrName.push(a.children[0].innerHTML);
         }
     });
-    marksArrName.forEach((a, index) => {
+    marksArrName.forEach((a, i) => {
         let temp = new DOMParser().parseFromString(a, 'text/html').body;
         temp.removeChild(temp.getElementsByTagName('b')[0]);
         temp.removeChild(temp.getElementsByTagName('br')[0]);
-        marksArrName[index] = temp.innerHTML;
+        marksArrName[i] = temp.innerHTML;
     });
-    marksArrNum.forEach((e, index) => {
+    marksArrName.forEach((a, index) => {
+        marksString += marksArrName[index] + '&#09;' + marksArrNum[index] + '\n';
+    });
+    marksString = marksString.replace(/\n$/, '');
+    textArea.innerHTML = marksString;
+
+
+
+    //All failed marks
+    let fail = cH3(),
+        failedText = document.createElement('textarea'),
+        failedNums = [],
+        failedNames = [],
+        failedString = '';
+    fail.innerHTML = text.fail;
+    marksArrNum.forEach((e, i) => {
         if (e < 4) {
             failedNums.push(e);
-            failedNames.push(marksArrName[index]);
+            failedNames.push(marksArrName[i]);
         }
     });
-    let marksObj = '';
-    marksArrName.forEach((a, index) => {
-        marksObj += marksArrName[index] + '&#09;' + marksArrNum[index] + '\n';
-    });
-    marksObj = marksObj.replace(/\n$/, '');
-    let textArea = document.createElement('textarea'),
-        all = cH3(),
-        fail = cH3(),
-        failedText = document.createElement('textarea'),
-        small = document.createElement('small'),
-        avgH3 = cH3(),
-        avgDiv = cDiv(),
-        summary = cH3(),
-        sumDiv = cDiv(),
-        div = cDiv(),
-        compDubH3 = cH3(),
-        compDubDiv = cDiv(),
-        compDubDivP = cDiv(),
-        compDubDivN = cDiv(),
-        failedString = '',
-        compDub = 0,
-        compDubP = 0,
-        compDubN = 0,
-        numFailed = 0;
-    textArea.setAttribute('readonly', '');
-    textArea.setAttribute('onClick', 'this.focus();this.select()');
-    textArea.style.width = '300px';
-    textArea.style.outline = 'none';
-    fail.innerHTML = text.fail;
-    all.innerHTML = text.all;
-    all.style.marginBottom = '6px';
-    textArea.innerHTML = marksObj;
     failedNums.forEach((e, i) => {
         failedString += failedNames[i] + '&#09;' + failedNums[i] + '\n';
     });
-    marksArrNum.forEach(e => {
-        if (e < 4) compDub += (e - 4) * 2;
-        else compDub += e - 4;
-    });
+    failedString = failedString.replace(/\n$/, '');
+    fail.style.marginBottom = '2px';
+    failedText.innerHTML = failedString;
+
+
+
+    //Compensate Double
+    let compDubH3 = cH3(),
+        compDubDiv = cDiv(),
+        compDubDivP = cDiv(),
+        compDubDivN = cDiv(),
+        compDub = 0,
+        compDubP = 0,
+        compDubN = 0;
     marksArrNum.forEach(e => {
         if (e < 4) compDubN += (e - 4) * 2;
         else compDubP += e - 4;
     });
+    compDub = compDubP + compDubN;
     if (compDubN == 0) compDubN = '-' + compDubN;
-    compDubH3.innerHTML = text.compDub + '&nbsp;';
     if (compDub < 0) red(compDubDiv);
     else green(compDubDiv);
-    compDubN = String(compDubN).replace('-', '- ');
-    styler(compDubDiv);
-    styler(compDubDivP);
-    styler(compDubDivN);
-    green(compDubDivP);
-    compDubDivP.innerHTML = '+ ' + compDubP;
+    if (compDub == 0) compDubDiv.style.color = 'orange';
+    compDubN = String(compDubN).replace('-', '-&nbsp;');
     red(compDubDivN);
     compDubDivN.innerHTML = compDubN;
+    compDubDivP.innerHTML = '+&nbsp;' + compDubP;
+    green(compDubDivP);
     compDubDiv.innerHTML = compDub;
-    console.log(compDubDiv);
+    compDubH3.innerHTML = text.compDub + '&nbsp;';
     compDubH3.appendChild(compDubDivP);
     compDubH3.innerHTML += '&nbsp;';
     compDubH3.appendChild(compDubDivN);
     compDubH3.innerHTML += '&nbsp;=&nbsp;';
     compDubH3.appendChild(compDubDiv);
-    failedString = failedString.replace(/\n$/, '');
+
+
+
+    let div = cDiv(),
+        small = document.createElement('small'),
+        numFailed = 0;
     if (failedString.split('\n')[0]) numFailed = failedString.split('\n').length;
-    failedText.innerHTML = failedString;
-    failedText.setAttribute('readonly', '');
-    failedText.setAttribute('onClick', 'this.focus();this.select()');
-    failedText.style.width = '300px';
-    failedText.style.outline = 'none';
-    fail.style.marginBottom = '2px';
-    small.innerHTML = text.failSmall + '&nbsp;';
-    div.innerHTML = numFailed;
     if (numFailed > 3) red(div);
     else green(div);
-    styler(div);
+    div.innerHTML = numFailed;
+    small.innerHTML = text.failSmall + '&nbsp;';
     small.appendChild(div);
 
-    function insert(Arr) {
-        window.elRemove = Arr;
-        for (let i = 0; i < Arr.length; i++) {
-            page.insertBefore(Arr[i], pageChildren[indexH3 + 1]);
-        }
-    }
-    let avg = 0;
+
+
+    let avg = 0,
+        avgH3 = cH3(),
+        avgDiv = cDiv();
     marksArrNum.forEach(e => {
         avg += +e;
     });
@@ -197,10 +211,13 @@ window.grabMarks = () => {
     avgDiv.innerHTML = avg;
     if (avg < 4) red(avgDiv);
     else green(avgDiv);
-    styler(avgDiv);
     avgH3.appendChild(avgDiv);
-    styler(sumDiv);
-    if (numFailed > 3 || avg < 4 || compDub < 0) {
+
+
+
+    let summary = cH3(),
+        sumDiv = cDiv();
+    if (numFailed > 3 || compDub < 0) {
         sumDiv.innerHTML = text.sumF;
         red(sumDiv);
     } else {
@@ -209,12 +226,17 @@ window.grabMarks = () => {
     }
     summary.innerHTML = text.sum + '&nbsp;';
     summary.appendChild(sumDiv);
+
     insert([br(), br(), summary, all, textArea, br(), br(), fail, small, br(), br(), failedText, br(), br(), avgH3, compDubH3]);
-    textArea.style.height = textArea.scrollHeight + 'px';
-    failedText.style.height = failedText.scrollHeight + 'px';
+
+    textStyler(textArea);
+    textStyler(failedText);
     button.innerHTML = text.remove;
     button.setAttribute('onclick', 'removeBut()');
 };
+
+
+
 window.removeBut = () => {
     window.elRemove.forEach(e => {
         e.parentElement.removeChild(e);
