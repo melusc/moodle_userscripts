9659bb3ee69814e56c913c6ddf823fa18def696e
melusc <61409445+melusc@users.noreply.github.com>
2020-04-04 17:41:34 +0200

    :fire:

--- Clean Moodle.user.js
+++ Clean Moodle.user.js
@@ -1,8 +1,9 @@
 // ===UserScript===
 // @name        Clean Moodle
 // @namespace   https://github.com/melusc/lusc
-// @version     3.5.2
+// @version     2020.04.04a
 // @include     *://moodle.ksasz.ch/*
+// @exclude     *://moodle.ksasz.ch/online*
 // @Author      lusc
 // @description Improving the looks of Moodle
 // @downloadURL https://github.com/melusc/lusc/raw/master/Clean%20Moodle.user.js
@@ -18,67 +19,64 @@ run('replace', '...', '---'); //Replaces the name of the course "..." with "---"
 run('remove', '...'); //Removes the course "..."
 
 if (window.location.href == 'https://moodle.ksasz.ch/') {
-	cleanLinks('https://example.com/egg-sample'); //Removes the link to https://example.com/egg-sample from the sidebar
+    cleanLinks('https://example.com/egg-sample'); //Removes the link https://example.com/egg-sample from the right sidebar
 }
 
 //Code
-function run(custom, customReplace, customReplaceWith) {
-    let thisHeading = document.querySelector('.type_system.depth_2.contains_branch').querySelector(`[title="${customReplace}"]`);
+function run(what, selector, replace) {
+    let thisHeading = document.querySelector('.type_system.depth_2.contains_branch').querySelector(`[title="${selector}"]`);
     if (!thisHeading) {
-        thisHeading = document.querySelector('.type_system.depth_2.contains_branch').querySelector(`[title="${customReplace} "]`);
+        thisHeading = document.querySelector('.type_system.depth_2.contains_branch').querySelector(`[title="${selector} "]`);
     }
-    if (custom === 'remove') {
+    if (what === 'remove') {
         if (thisHeading) {
-            thisHeading = thisHeading.parentNode.parentNode;
-            thisHeading.parentNode.removeChild(thisHeading);
+            thisHeading = thisHeading.parentElement.parentElement;
+            thisHeading.parentElement.removeChild(thisHeading);
         } else if (document.querySelector('.block_navigation.block')) {
-            alert(`Error removing "${customReplace}"! Check if it's written correctly or if you might be missing a whitespace at the end.`);
+            alert(`Error removing "${selector}"! Check if it's written correctly or if you might be missing a whitespace at the end.`);
         }
-    } else if (custom === 'replace') {
-        if (document.querySelector('.block_navigation.block') && !thisHeading) {
-            alert(`Error replacing "${customReplace}"! Check if it's written correctly or if you might be missing a whitespace at the end.`);
-        } else if (thisHeading.parentNode.parentNode.className.startsWith('type_course depth_3 i')) {
-            thisHeading.children[1].innerHTML = customReplaceWith;
-        } else if (thisHeading.parentNode.parentNode.className.startsWith('type_course depth_3 c')) {
-            thisHeading.innerHTML = customReplaceWith;
+    } else if (what === 'replace') {
+        if (document.getElementsByClassName('block_navigation block')[0] && !thisHeading) {
+            alert(`Error replacing "${selector}"! Check if it's written correctly or if you might be missing a whitespace at the end.`);
+        } else if (thisHeading.parentElement.parentElement.className.startsWith('type_course depth_3 i')) {
+            thisHeading.children[1].innerHTML = replace;
+        } else if (thisHeading.parentElement.parentElement.className.startsWith('type_course depth_3 c')) {
+            thisHeading.innerHTML = replace;
         }
-    } else if (document.querySelector('.block_navigation.block')) {
-        alert(`Unable to "${custom}" "${customReplace}"`);
+    } else if (document.getElementsByClass('block_navigation block')[0]) {
+        alert(`Unable to "${what}" "${selector}"`);
     }
 }
 let arrayUS = [];
 if (sortAlphabetically) {
     let i = 0;
-    while (document.querySelector('.type_system.depth_2.contains_branch').children[1].children[i]) {
-        if (document.querySelector('.type_system.depth_2.contains_branch').children[1].children[i].className == 'type_course depth_3 item_with_icon') {
-            arrayUS.push(document.querySelector('.type_system.depth_2.contains_branch').children[1].children[i].children[0].children[0].children[1].innerHTML);
-        } else if (document.querySelector('.type_system.depth_2.contains_branch').children[1].children[i].className.startsWith('type_course depth_3 contains_branch')) {
-            arrayUS.push(document.querySelector('.type_system.depth_2.contains_branch').children[1].children[i].children[0].children[0].innerHTML);
+    let doc = Array.from(document.querySelector('.type_system.depth_2.contains_branch').children[1].children);
+    doc.forEach((e, i) => {
+        if (doc[i].className == 'type_course depth_3 item_with_icon') {
+            arrayUS.push(doc[i].children[0].children[0].children[1].innerHTML)
+        } else if (doc[i].className.startsWith('type_course depth_3 contains_branch')) {
+            arrayUS.push(doc[i].children[0].children[0].innerHTML)
         }
-        i++;
-    }
+    });
     let arrayS = arrayUS.slice().sort(),
-        arrayDoc = [],
-        j;
-    for (j = 0; arrayUS.length > j; j++) {
-        arrayDoc.push(document.querySelector('.type_system.depth_2.contains_branch').children[1].children[arrayUS.findIndex((element) => element == arrayS[j])]);
-    }
-    let k;
-    for (k = 0; arrayUS.length > k; k++) {
-        let doc = arrayDoc[k];
-        let docOther = document.querySelector('.type_system.depth_2.contains_branch').children[1].children[k + 1];
-        document.querySelector('.type_system.depth_2.contains_branch').children[1].insertBefore(doc, docOther);
-    }
+        arrayDoc = [];
+    arrayUS.forEach((e, i) => {
+        arrayDoc.push(document.querySelector('.type_system.depth_2.contains_branch').children[1].children[arrayUS.indexOf(arrayS[i])]);
+    });
+    arrayUS.forEach((e, i) => {
+        let docOther = document.querySelector('.type_system.depth_2.contains_branch').children[1].children[i + 1];
+        document.querySelector('.type_system.depth_2.contains_branch').children[1].insertBefore(arrayDoc[i], docOther);
+    });
 }
 
-function cleanLinks(el) {
-    let nodes = document.querySelector('#inst161').querySelector(`[href="${el}"]`),
-        elementArray = Array.prototype.indexOf.call(nodes.parentElement.children, nodes);
-    if (elementArray >= 0) {
-        if (nodes.parentElement.childElementCount > elementArray + 1) {
-            nodes.parentElement.removeChild(nodes.parentElement.children[elementArray + 1]);
-            nodes.parentElement.removeChild(nodes.parentElement.children[elementArray]);
-        } else if (nodes.parentElement.childElementCount <= 1) nodes.parentElement.parentElement.removeChild(nodes.parentElement);
-        else nodes.parentElement.removeChild(nodes.parentElement.children[elementArray]);
+function cleanLinks(link) {
+    let e = document.getElementById('inst161').querySelector(`[href="${link}"]`),
+        elementArray = Array.from(e.parentElement.children).indexOf(e);
+    if (elementArray != -1) {
+        if (e.parentElement.childElementCount > elementArray + 1) {
+            e.parentElement.removeChild(e.parentElement.children[elementArray + 1]);
+            e.parentElement.removeChild(e.parentElement.children[elementArray]);
+        } else if (e.parentElement.childElementCount <= 1) e.parentElement.parentElement.removeChild(e.parentElement);
+        else e.parentElement.removeChild(e.parentElement.children[elementArray]);
     }
 }
