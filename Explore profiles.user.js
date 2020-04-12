0bda773d34fe369fa05b822e80080ca6c8ced3e5
melusc <61409445+melusc@users.noreply.github.com>
2020-04-12 13:50:53 +0200

    :fire:

--- Explore profiles.user.js
+++ Explore profiles.user.js
@@ -1,145 +1,128 @@
 // ==UserScript==
 // @name         Explore profiles
 // @namespace    https://github.com/melusc/lusc
-// @version      1.5
+// @version      2020.04.12a
 // @author       lusc
 // @match        *://moodle.ksasz.ch/user/profile.php*
 // @downloadURL  https://github.com/melusc/lusc/raw/master/Explore%20profiles.user.js
 // @updateURL    https://github.com/melusc/lusc/raw/master/Explore%20profiles.user.js
+// @grant        GM_addStyle
 // ==/UserScript==
-function getCookie(name) {
-  var value = "; " + document.cookie;
-  var parts = value.split("; " + name + "=");
-  if (parts.length == 2) return parts.pop().split(";").shift();
-}
-let style = document.createElement("style");
-style.innerHTML += `
-input.buttons {
-    border: 1px solid #1a2027!important;
-    border-radius: 2px;
-    background-color: #fff;
-    background-image: linear-gradient(to bottom,#fff,#fff);
-    color: #1a2027;
-    text-shadow: 0 1px 1px rgba(255,255,255,0)!important;
-    box-shadow: inset 0 1px 0 transparent,0 1px 2px transparent;
-}
-input.buttons:hover {
-    background-color: #e5e5e5;
-    color: #30363c;
-    background-image: linear-gradient(to bottom,#e5e5e5,#e5e5e5);
-}`;
-document.head.appendChild(style);
 
-let url = Number(new URLSearchParams(window.location.search).get('id'));
-if (!url) {
-    url = 1612;
+
+/*
+ *     Tried and tested with Tampermonkey on Chrome
+ */
+
+
+
+GM_addStyle(`
+button{
+   border:1px solid white;
+   border-radius:2px;
+   color:white;
+   background-color:transparent;
+   background-image:linear-gradient(to bottom,transparent,transparent);
+   text-shadow:0 1px 1px transparent;
 }
-let randNum = Math.floor((Math.random() * 1744) + 2);
-randNum1();
-function randNum1(){
-    if (randNum < 101 && randNum > 76) {
-        randNum = Math.floor((Math.random() * 1744) + 2);
-        randNum1();
-    } else if (randNum < 630 && randNum > 112) {
-        randNum = Math.floor((Math.random() * 1744) + 2);
-        randNum1();
-    } else if (randNum < 779 && randNum > 659) {
-        randNum = Math.floor((Math.random() * 1744) + 2);
-        randNum1();
-    } else if (randNum < 817 && randNum > 800) {
-        randNum = Math.floor((Math.random() * 1744) + 2);
-        randNum1();
-    } else if (randNum < 885 && randNum > 828) {
-        randNum = Math.floor((Math.random() * 1744) + 2);
-        randNum1();
-    } else if (randNum < 927 && randNum > 911) {
-        randNum = Math.floor((Math.random() * 1744) + 2);
-        randNum1();
-    } else if (randNum < 945 && randNum > 935) {
-        randNum = Math.floor((Math.random() * 1744) + 2);
-        randNum1();
-    } else if (randNum < 978 && randNum > 967) {
-        randNum = Math.floor((Math.random() * 1744) + 2);
-        randNum1();
-    } else if (randNum < 1038 && randNum > 1004) {
-        randNum = Math.floor((Math.random() * 1744) + 2);
-        randNum1();
-    } else if (randNum < 1203 && randNum > 1191) {
-        randNum = Math.floor((Math.random() * 1744) + 2);
-        randNum1();
+button:hover{
+   background-color:transparent;
+   background-image:linear-gradient(to bottom,transparent,transparent);
+   color:white;
+   filter: brightness(0.75);
+}`);
+let params;
+if(document.getElementsByClassName('alert alert-error alert-block fade in ')[0]){
+    if(localStorage.getItem('action')){
+        console.log('Del');
+        console.log(localStorage.getItem('action'));
+        explorer(false,localStorage.getItem('action'));
     }
 }
-let prevUrl = `https://moodle.ksasz.ch/user/profile.php?id=${url - 1}`,
-    nextUrl = `https://moodle.ksasz.ch/user/profile.php?id=${url + 1}`,
-    prevUrl10 = `https://moodle.ksasz.ch/user/profile.php?id=${url - 10}`,
-    nextUrl10 = `https://moodle.ksasz.ch/user/profile.php?id=${url + 10}`,
-    randUrl = `https://moodle.ksasz.ch/user/profile.php?id=${randNum}`,
-    action = getCookie('action'),
-    form = document.createElement('form'),
-    prev = document.createElement('input'),
-    next = document.createElement('input'),
-    prev10 = document.createElement('input'),
-    next10 = document.createElement('input'),
-    rand = document.createElement('input');
-
-prev.setAttribute('type','button');
-prev.setAttribute('value','Previous profile');
-prev.setAttribute('class','buttons');
-prev.setAttribute('onclick',`window.location.href='${prevUrl}';document.cookie = 'action=-1';`);
-form.appendChild(prev);
-
-next.setAttribute('type','button');
-next.setAttribute('value','Next profile');
-next.setAttribute('class','buttons');
-next.setAttribute('onclick',`window.location.href='${nextUrl}';document.cookie = 'action=+1';`);
-form.appendChild(next);
-
-prev10.setAttribute('type','button');
-prev10.setAttribute('value','-10 profiles');
-prev10.setAttribute('class','buttons');
-prev10.setAttribute('onclick',`window.location.href='${prevUrl10}';document.cookie = 'action=-10';`);
-form.appendChild(prev10);
-
-next10.setAttribute('type','button');
-next10.setAttribute('value','+10 profiles');
-next10.setAttribute('class','buttons');
-next10.setAttribute('onclick',`window.location.href='${nextUrl10}';document.cookie = 'action=+10';`);
-form.appendChild(next10);
-
-rand.setAttribute('type','button');
-rand.setAttribute('value','Random profile');
-rand.setAttribute('class','buttons');
-rand.setAttribute('onclick',`window.location.href='${randUrl}';document.cookie = 'action=rand';`);
-form.appendChild(rand);
-form.setAttribute('style','display:inline');
-form.id = 'exploreProfiles';
-
-const whitespace = document.createElement('span');
-whitespace.innerHTML = '&nbsp;&nbsp;&nbsp;';
-
-document.querySelector('.page-header-headings').appendChild(whitespace);
-document.querySelector('.page-header-headings').appendChild(form);
-document.querySelector('.page-header-headings').children[0].setAttribute('style','display:inline');
-
-var doc = document.querySelector('.alert').innerHTML;
-if (doc) {
-    if (url <= 1745) {
-        if (url >= 2) {
-            if (action == +1) {
-                window.open(nextUrl,'_self');
-            } else if (action == -1) {
-                window.open(prevUrl,'_self');
-            } else if (action == +10) {
-                window.open(nextUrl10,'_self');
-            } else if (action == -10) {
-                window.open(prevUrl10,'_self');
-            } else if (action == 'rand') {
-                window.open(randUrl,'_self');
-            }
-        } else {
-            window.open(`https://moodle.ksasz.ch/user/profile.php?id=1745`,'_self');
-        }
+function c(e){
+    return document.createElement(e);
+}
+let button1 = c('button'),
+    button2 = c('button'),
+    button3 = c('button'),
+    button4 = c('button'),
+    button5 = c('button'),
+    div = c('div');
+button1.id = "+1";
+button2.id = "-1";
+button3.id = "rand";
+button4.id = "+10";
+button5.id = "-10";
+button1.innerHTML = 'Next profile';
+button2.innerHTML = 'Previous profile';
+button3.innerHTML = 'Random profile';
+button4.innerHTML = '+10 profiles';
+button5.innerHTML = '-10 profiles';
+div.appendChild(button2);
+div.appendChild(button1);
+div.appendChild(button3);
+div.appendChild(button5);
+div.appendChild(button4);
+let buttons = div.getElementsByTagName('button');
+for (let i = 0, length = buttons.length; i < length; i++){
+    buttons[i].onclick = e=>{
+        explorer(e.target);
+    };
+}
+let header = document.getElementsByClassName('page-header-headings')[0];
+header.appendChild(div);
+function explorer(e,action){
+    if (!action){
+        action = +e.id;
+    }
+    action = +action;
+    console.log(action);
+    let url = window.location;
+        params = new URLSearchParams(url.search.slice(1));
+    if(isNaN(action)){
+        randNum();
     } else {
-        window.open(`https://moodle.ksasz.ch/user/profile.php?id=2`,'_self');
+        let id = +params.get('id');
+        params.set('id',id + action);
+    }
+    if(e){
+        localStorage.setItem('action',e.id);
+    }
+    window.location.search = '?' + params.toString();
+}
+function randNum(){
+    let num = Math.floor(Math.random()*1745 + 2);
+    console.log(num);
+    if /***/(num > 76 && 101 > num) randNum();
+    else if (num > 112 && 630 > num) randNum();
+    else if (num > 630 && 636 > num) randNum();
+    else if (num > 644 && 649 > num) randNum();
+    else if (num > 651 && 659 > num) randNum();
+    else if (num > 659 && 779 > num) randNum();
+    else if (num > 779 && 784 > num) randNum();
+    else if (num > 784 && 800 > num) randNum();
+    else if (num > 800 && 817 > num) randNum();
+    else if (num > 828 && 885 > num) randNum();
+    else if (num > 885 && 894 > num) randNum();
+    else if (num > 894 && 903 > num) randNum();
+    else if (num > 905 && 911 > num) randNum();
+    else if (num > 911 && 927 > num) randNum();
+    else if (num > 927 && 935 > num) randNum();
+    else if (num > 935 && 945 > num) randNum();
+    else if (num > 945 && 954 > num) randNum();
+    else if (num > 954 && 959 > num) randNum();
+    else if (num > 959 && 967 > num) randNum();
+    else if (num > 967 && 978 > num) randNum();
+    else if (num > 978 && 987 > num) randNum();
+    else if (num > 988 && 994 > num) randNum();
+    else if (num > 994 && 999 > num) randNum();
+    else if (num > 1004 && 1038 > num) randNum();
+    else if (num > 1038 && 1045 > num) randNum();
+    else if (num > 1045 && 1050 > num) randNum();
+    else if (num > 1185 && 1190 > num) randNum();
+    else if (num > 1191 && 1203 > num) randNum();
+    else if (num > 1367 && 1375 > num) randNum();
+    else {
+        params.set('id',num);
     }
 }
