67a9d5290dfb9b7da9499c955995a91490ad2155
melusc <61409445+melusc@users.noreply.github.com>
2020-04-16 16:48:38 +0200

    :fire:

--- Timetable in Moodle.user.js
+++ Timetable in Moodle.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Moodle Timetable
-// @version      2020.03.26d
+// @version      2020.04.16a
 // @author       lusc
 // @match        *://moodle.ksasz.ch/
 // @match        *://moodle.ksasz.ch/?
