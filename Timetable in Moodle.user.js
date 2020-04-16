4155bfcd6ef269318b173e0461a139b98c59b434
melusc <61409445+melusc@users.noreply.github.com>
2020-04-16 16:45:31 +0200

    :fire:

--- Timetable in Moodle.user.js
+++ Timetable in Moodle.user.js
@@ -1,39 +1,82 @@
 // ==UserScript==
-// @name         Timetable in Moodle
+// @name         Moodle Timetable
 // @version      2020.03.26d
-// @description  try to take over the world!
 // @author       lusc
 // @match        *://moodle.ksasz.ch/
 // @match        *://moodle.ksasz.ch/?
+// @match        *://moodle.ksasz.ch/?lang=*
 // @updateURL    https://github.com/melusc/lusc/raw/master/Timetable%20in%20Moodle.user.js
 // @downloadURL  https://github.com/melusc/lusc/raw/master/Timetable%20in%20Moodle.user.js
 // @grant        none
 // ==/UserScript==
 'use strict';
-
+let customTimeSlot = '';
 //Replace the {object} below with the object that you can get from https://melusc.github.io/lusc/Stundenplan
 let lessons = {
-    "1-1": "Example"
-},
+        '1-1': 'Biologie',
+        '1-2': 'Biologie',
+        '1-3': 'Sport',
+        '1-4': 'Physik',
+        '1-5': 'Physik',
+        '1-6': 'Mittag',
+        '1-7': 'Mathematik',
+        '1-8': 'Mathematik',
+        '2-1': 'Musik',
+        '2-2': 'Musik',
+        '2-3': 'Deutsch',
+        '2-4': 'Englisch',
+        '2-5': 'Englisch',
+        '2-6': 'Mittag',
+        '2-7': 'BG',
+        '2-8': 'BG',
+        '3-1': 'Französisch',
+        '3-2': 'Französisch',
+        '3-3': 'Klassenstunde',
+        '3-4': 'Englisch',
+        '3-5': 'Mittag',
+        '3-6': 'Mittag',
+        '3-7': 'Deutsch',
+        '3-8': 'Deutsch',
+        '4-1': 'Chemie',
+        '4-2': 'Chemie',
+        '4-3': 'Arbeitstechnik',
+        '4-4': 'Geografie',
+        '4-5': 'Geografie',
+        '4-6': 'Mittag',
+        '4-7': 'Wirtschaft und Recht',
+        '4-8': 'Wirtschaft und Recht',
+        '4-9': 'Wirtschaft und Recht',
+        '5-1': 'Mathematik',
+        '5-2': 'Mathematik',
+        '5-3': 'Sport',
+        '5-4': 'Sport',
+        '5-5': 'Französisch',
+        '5-6': 'Mittag',
+        '5-7': 'Geschichte',
+        '5-8': 'Geschichte'
+    },
     times = {
-    1:['8:00','8:45'],
-    2:['8:45','9:30'],
-    3:['9:50','10:35'],
-    4:['10:40','11:25'],
-    5:['11:30','12:15'],
-    6:['12:15','13:10'],
-    7:['13:10','13:55'],
-    8:['13:55','14:40'],
-    9:['14:50','15:35']
-},
+        1: ['8:00', '8:45'],
+        2: ['8:45', '9:30'],
+        3: ['9:50', '10:35'],
+        4: ['10:40', '11:25'],
+        5: ['11:30', '12:15'],
+        6: ['12:15', '13:10'],
+        7: ['13:10', '13:55'],
+        8: ['13:55', '14:40'],
+        9: ['14:50', '15:35']
+    },
     text = {
-    tt: 'Stundenplan',
-    nL: 'Kein Unterricht',
-    now: 'Jetzt',
-    after: 'Nachher',
-    break: 'Grosse Pause, dann',
-    wE: 'Wochenende!'
-};
+        tt: 'Stundenplan',
+        nL: 'Kein Unterricht',
+        nLA: 'Kein Unterricht mehr',
+        now: 'Jetzt',
+        after: 'Nachher',
+        break: 'Grosse Pause, dann',
+        wE: 'Wochenende',
+        next: 'Nächste Stunde',
+        cTS: 'customTimeSlot activated\nUse only for testing'
+    };
 
 function refresh() {
     timeTable();
@@ -54,7 +97,7 @@ function refreshAt(hours, minutes, seconds) {
     let now = new Date();
     let then = new Date();
 
-    if (now.getHours() > hours || (now.getHours() == hours && now.getMinutes() > minutes) || now.getHours() == hours && now.getMinutes() == minutes && now.getSeconds() >= seconds) {
+    if (now.getHours() > hours || (now.getHours() === hours && now.getMinutes() > minutes) || now.getHours() === hours && now.getMinutes() === minutes && now.getSeconds() >= seconds) {
         then.setDate(now.getDate() + 1);
     }
     then.setHours(hours);
@@ -65,7 +108,6 @@ function refreshAt(hours, minutes, seconds) {
     console.log(timeout);
     setTimeout(refresh, timeout);
 }
-
 let span = document.createElement('span'),
     strong = document.createElement('strong'),
     p = document.createElement('p'),
@@ -78,14 +120,14 @@ let span = document.createElement('span'),
     li = document.createElement('li'),
     p2 = document.createElement('p');
 span.innerHTML = text.tt;
-span.style = "font-size: medium;";
-div1.className = "no-overflow";
-div1.id = "currentLesson";
-div2.className = "no-overflow";
-div3.className = "contentwithoutlink ";
-div5.className = "mod-indent-outer";
-li.className = "activity label modtype_label ";
-li.id = "module-currentClass";
+span.style = 'font-size: medium;';
+div1.className = 'no-overflow';
+div1.id = 'currentLesson';
+div2.className = 'no-overflow';
+div3.className = 'contentwithoutlink ';
+div5.className = 'mod-indent-outer';
+li.className = 'activity label modtype_label ';
+li.id = 'module-currentClass';
 strong.appendChild(span);
 p.appendChild(strong);
 div1.appendChild(p);
@@ -115,48 +157,89 @@ function timeTable() {
     for (let k = 1; k <= Object.keys(timesArr).length; k++) {
         timesMin[k] = timesArr[k][0] * 60 + Number(timesArr[k][1]);
     }
-    for (let i = 1; i <= Object.keys(times).length; i++){
+
+    for (let i = 1; i <= Object.keys(times).length; i++) {
         if (minute >= timesMin[i] && minute <= timesMin[i + 1]) timeSlot = i;
     }
     if (minute < timesMin['1']) timeSlot = 0;
-    else if (minute > timesMin[Object.keys(times).length] + 1) timeSlot = -1;
+    else if (minute > timesMin[Object.keys(times).length + 1]) timeSlot = -1;
 
+    if (customTimeSlot) {
+        timeSlot = +customTimeSlot;
+        alert(text.cTS);
+    }
     let currentLesson,
         nextLesson,
-        colour = window.getComputedStyle( document.querySelector('#label_1_1') ,null).getPropertyValue('color'),
+        colour = window.getComputedStyle(document.querySelector('#label_1_1'), null).getPropertyValue('color'),
         time1,
-        time2;
+        time2,
+        both;
     currentLesson = lessons[`${day}-${timeSlot}`];
     nextLesson = lessons[`${day}-${timeSlot + 1}`];
+    if (!nextLesson && !currentLesson) both = true;
     if (!nextLesson) {
         nextLesson = text.nL;
         time2 = `<div style="color:${colour};font-weight:400;display:inline">:</div>`;
+    } else {
+        time2 = '<div style="color:white;display:inline;font-weight:400;"> (' + times[timeSlot + 1][0] + '-' + times[timeSlot + 1][1] + '):</div>';
     }
-    else {
-        time2 = '<div style="color:white;display:inline;font-weight:400;"> (' + times[timeSlot + 1][0] + "-" + times[timeSlot + 1][1] + "):</div>";
-    }
+    let links = Array.from(document.getElementsByClassName('type_system depth_2 contains_branch')[0].getElementsByClassName('type_course depth_3 item_with_icon')),
+        currentLink,
+        nextLink;
+    links.forEach(e => {
+        if (e.getElementsByTagName('span')[0].innerHTML == currentLesson) currentLink = e;
+        if (e.getElementsByTagName('span')[0].innerHTML == nextLesson) nextLink = e;
+    });
+    if (currentLink) currentLink = currentLink.getElementsByTagName('a')[0].href;
+    if (nextLink) nextLink = nextLink.getElementsByTagName('a')[0].href;
     if (!currentLesson) {
-        currentLesson = text.nL;
+        if (!both) currentLesson = text.nL;
+        else currentLesson = text.nLA;
         time1 = `<div style="color:${colour};font-weight:400;display:inline">:</div>`;
+    } else {
+        time1 = '<div style="color:white;display:inline;font-weight:400;"> (' + times[timeSlot][0] + '-' + times[timeSlot][1] + '):';
     }
-    else {
-        time1 = '<div style="color:white;display:inline;font-weight:400;"> (' + times[timeSlot][0] + "-" + times[timeSlot][1] + "):";
-    }
-    if (day < 6) {
-        document.querySelector('#currentLesson').children[1].innerHTML = `
+    if (day != 6 && day != 0) {
+        let x = `
 <table>
     <tbody style="font-size: large;">
         <tr>
             <th style="color: ${colour};">${text.now}${time1}</th>
-            <td style="padding-left: 10px;">${currentLesson}</td>
-        </tr>
+            <td style="padding-left: 10px;">`;
+
+        if (currentLink) {
+            x += `
+                <a href="${currentLink}">${currentLesson}</a>
+            </td>`;
+        } else {
+            x += currentLesson + '</td>';
+        }
+
+        x += `
+        </tr>`;
+
+        if (!both) {
+            x += `
         <tr>
             <th style="color: ${colour};">${text.after}${time2}</th>
-            <td style="padding-left: 10px;">${nextLesson}</td>
-        </tr>
+            <td style="padding-left: 10px;">`;
+
+            if (nextLink) {
+                x += `
+                <a href="${nextLink}">${nextLesson}</a>
+            </td>`;
+            } else {
+                x += nextLesson + '</td>';
+            }
+
+            x += `
+        </tr>`;
+        }
+        x += `
     </tbody>
 </table>`;
-    } else {
-        document.querySelector('#currentLesson').children[1].innerHTML = `<strong style="${colour}; font-size:large;">${text.wE}</strong>`;
+        document.querySelector('#currentLesson').children[1].innerHTML = x;
+    } else if (window.location.href === 'https://moodle.ksasz.ch/') {
+        document.querySelector('#currentLesson').children[1].innerHTML = `<strong style="color:${colour}; font-size:large;">${text.wE}</strong>`;
     }
 }
