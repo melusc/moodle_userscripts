1cb2388839fd7878db5545316dcf663aae03cf25
melusc <61409445+melusc@users.noreply.github.com>
2020-05-21 15:57:13 +0200

    :fire:

--- Clean Moodle.user.js
+++ Clean Moodle.user.js
@@ -1,7 +1,7 @@
 // ===UserScript===
 // @name        Clean Moodle
 // @namespace   https://github.com/melusc/lusc
-// @version     2020.05.18a
+// @version     2020.05.21a
 // @include     *://moodle.ksasz.ch/*
 // @exclude     *://moodle.ksasz.ch/info*
 // @exclude     *://moodle.ksasz.ch/lib*
@@ -37,8 +37,8 @@ function sort(e, sidebar) {
             const li = Array.from(sidebar.children[1].getElementsByClassName('type_course depth_3'));
 
             li.sort((a, b) => {
-                a = a.textContent;
-                b = b.textContent;
+                a = a.textContent.toLowerCase();
+                b = b.textContent.toLowerCase();
                 if (a < b) return -1;
                 else if (a > b) return 1;
                 else return 0;
@@ -47,12 +47,12 @@ function sort(e, sidebar) {
                 sidebar.children[1].appendChild(li[i]);
             }
         }
-        console.log('Clean moodle version ' + GM_info.script.version + ' by lusc');
+        console.log('Clean moodle version %c' + GM_info.script.version + ' %cby %clusc', 'color: #fe4c4c', 'color:', 'color:#58e');
     }
 }
 
 function runner(sidebar) {
-    let removeArr = GM_getValue('remove'),
+    const removeArr = GM_getValue('remove'),
         replaceArr = GM_getValue('replace');
     for (let i = 0; i < removeArr.length; i++) {
         remove(removeArr[i], sidebar);
@@ -98,9 +98,7 @@ if (location.pathname.toLowerCase() !== '/cleanmoodle') {
     GM_addValueChangeListener('remove', (a, b, c, remote) => {
         if (remote) {
             fetch('/')
-                .then(e => {
-                    return e.text();
-                })
+                .then(e => e.text())
                 .then(e => {
                     e = new DOMParser().parseFromString(e, 'text/html');
                     document.getElementById('inst4').outerHTML = e.getElementById('inst4').outerHTML;
@@ -112,9 +110,7 @@ if (location.pathname.toLowerCase() !== '/cleanmoodle') {
     GM_addValueChangeListener('replace', (a, b, c, remote) => {
         if (remote) {
             fetch('/')
-                .then(e => {
-                    return e.text();
-                })
+                .then(e => e.text())
                 .then(e => {
                     e = new DOMParser().parseFromString(e, 'text/html');
                     document.getElementById('inst4').outerHTML = e.getElementById('inst4').outerHTML;
@@ -126,12 +122,12 @@ if (location.pathname.toLowerCase() !== '/cleanmoodle') {
 }
 
 if (location.pathname == '/user/preferences.php') {
-    let element = document.getElementById('maincontent').parentElement.children[2].children[0].cloneNode(true),
+    const element = document.getElementById('maincontent').parentElement.children[2].children[0].cloneNode(true),
         temp = element.getElementsByClassName('card-text')[0];
     element.getElementsByTagName('h4')[0].innerHTML = 'Clean Moodle';
 
     while (temp.children.length != 1) temp.removeChild(temp.lastChild);
-    let a = temp.children[0].children[0];
+    const a = temp.children[0].children[0];
     a.href = 'https://moodle.ksasz.ch/cleanMoodle/';
     a.setAttribute('target', '_blank');
     a.innerHTML = 'Settings';
@@ -143,9 +139,6 @@ if (location.pathname.toLowerCase().startsWith('/cleanmoodle')) {
     history.replaceState({}, '', '/cleanMoodle/');
     setup(true);
 }
-const hr = () => {
-    return c('hr');
-};
 
 function setup(newPage) {
     if (location.protocol !== 'https:') {
@@ -153,7 +146,7 @@ function setup(newPage) {
     }
     document.title = 'Clean Moodle Setup';
 
-    let icon = c('link');
+    const icon = c('link');
     icon.setAttribute('rel', 'shortcut icon');
     icon.href = 'https://moodle.ksasz.ch/theme/image.php/classic/theme/1588340020/favicon';
     document.head.appendChild(icon);
@@ -164,7 +157,7 @@ function setup(newPage) {
         }
     }
 
-    let style = c('link');
+    const style = c('link');
     style.setAttribute('rel', 'stylesheet');
     style.setAttribute('type', 'text/css');
     style.href = 'https://moodle.ksasz.ch/theme/styles.php/classic/1588340020_1588339031/all';
@@ -180,16 +173,14 @@ function setup(newPage) {
         }
     `);
     fetch('https://moodle.ksasz.ch/')
-        .then(e => {
-            return e.text();
-        })
+        .then(e => e.text())
         .then(e => {
             if (!newPage) {
                 while (document.body.lastChild) {
                     document.body.removeChild(document.body.lastChild);
                 }
             }
-            let response = new DOMParser().parseFromString(e, 'text/html'),
+            const response = new DOMParser().parseFromString(e, 'text/html'),
                 login = response.getElementById('login');
             if (login) {
                 confirm('You are logged out\nLogin, return and reload page');
@@ -197,7 +188,7 @@ function setup(newPage) {
                 return false;
             } else {
 
-                let sidebar = response.getElementById('inst4'),
+                const sidebar = response.getElementById('inst4'),
                     content = response.getElementById('region-main'),
                     x = content.getElementsByClassName('section img-text')[0];
                 sort(true, sidebar.getElementsByClassName('type_system depth_2 contains_branch')[0]);
@@ -206,7 +197,7 @@ function setup(newPage) {
                     x.removeChild(x.lastChild);
                 }
 
-                let selectRemove = c('select'),
+                const selectRemove = c('select'),
                     elements = sidebar.getElementsByClassName('type_system depth_2 contains_branch')[0].children[1].children,
                     defaultOptionRemove = c('option');
                 selectRemove.id = 'selectRemove';
@@ -217,18 +208,18 @@ function setup(newPage) {
                 selectRemove.appendChild(defaultOptionRemove);
                 loop1:
                     for (let i = 0; i < elements.length; i++) {
-                        let value = elements[i].getElementsByTagName('a')[0].title,
+                        const value = elements[i].getElementsByTagName('a')[0].title,
                             replaceArr = GM_getValue('replace');
                         loop2:
                             for (let i = 0; i < replaceArr.length; i++) {
                                 if (value.trim() == replaceArr[i][0].trim()) continue loop1;
                             }
-                        let option = c('option');
+                        const option = c('option');
                         option.value = value;
                         option.innerHTML = value;
                         selectRemove.appendChild(option);
                     }
-                let removeLi = c('li'),
+                const removeLi = c('li'),
                     removeButton = c('button'),
                     clearRemoveButton = c('button'),
                     removeH3 = c('h3'),
@@ -239,7 +230,7 @@ function setup(newPage) {
                     replaceH3 = c('h3');
 
 
-                removeLi.appendChild(hr());
+                removeLi.appendChild(c('hr'));
 
                 removeH3.innerHTML = 'Removers';
                 removeLi.appendChild(removeH3);
@@ -263,12 +254,12 @@ function setup(newPage) {
                 content.getElementsByClassName('section img-text')[0].appendChild(removeLi);
 
 
-                replaceLi.appendChild(hr());
+                replaceLi.appendChild(c('hr'));
 
                 replaceH3.innerHTML = 'Replacers';
                 replaceLi.appendChild(replaceH3);
 
-                let selectReplace = c('select'),
+                const selectReplace = c('select'),
                     defaultOptionReplace = c('option');
                 selectReplace.id = 'selectReplace';
                 defaultOptionReplace.setAttribute('selected', true);
@@ -278,7 +269,7 @@ function setup(newPage) {
                 selectReplace.appendChild(defaultOptionReplace);
                 loop1:
                     for (let i = 0; i < elements.length; i++) {
-                        let option = c('option'),
+                        const option = c('option'),
                             value = elements[i].getElementsByTagName('a')[0].title,
                             replaceArr = GM_getValue('replace');
                         loop2:
@@ -289,7 +280,7 @@ function setup(newPage) {
                         option.innerHTML = value;
                         selectReplace.appendChild(option);
                     }
-                let input = c('input');
+                const input = c('input');
                 input.placeholder = 'Replace with';
                 input.id = 'inputReplace';
                 input.style.display = 'block';
@@ -317,38 +308,38 @@ function setup(newPage) {
                 document.body.appendChild(content);
 
 
-                let aside = response.getElementById('block-region-side-pre').cloneNode(false);
+                const aside = response.getElementById('block-region-side-pre').cloneNode(false);
                 aside.appendChild(sidebar);
 
-                let section = response.querySelector('section.hidden-print').cloneNode(false);
+                const section = response.querySelector('section.hidden-print').cloneNode(false);
                 section.appendChild(aside);
 
-                let div = c('div');
+                const div = c('div');
                 div.className = 'columnleft blockcolumn  has-blocks ';
                 div.appendChild(section);
 
-                let div5 = response.getElementById('region-main-box').cloneNode(false);
+                const div5 = response.getElementById('region-main-box').cloneNode(false);
                 div5.appendChild(content);
 
-                let div2 = response.getElementById('page-content').cloneNode(false);
+                const div2 = response.getElementById('page-content').cloneNode(false);
                 div2.appendChild(div5);
                 div2.appendChild(div);
 
-                let div3 = response.getElementById('page').cloneNode(false);
+                const div3 = response.getElementById('page').cloneNode(false);
                 div3.appendChild(div2);
 
-                let div4 = response.getElementById('page-wrapper').cloneNode(false);
+                const div4 = response.getElementById('page-wrapper').cloneNode(false);
                 div4.appendChild(div3);
 
                 document.body.appendChild(div4);
 
 
                 if (content) {
-                    let removeArr = GM_getValue('remove'),
+                    const removeArr = GM_getValue('remove'),
                         removeButtonsLi = c('li');
-                    removeButtonsLi.appendChild(hr());
+                    removeButtonsLi.appendChild(c('hr'));
                     for (let i = 0; i < removeArr.length; i++) {
-                        let removeButton = c('button');
+                        const removeButton = c('button');
                         removeButton.innerHTML = 'Stop removing "' + removeArr[i] + '"';
                         removeButton.addEventListener('click', removeRemover);
                         removeButton.dataset.id = removeArr[i];
@@ -356,11 +347,11 @@ function setup(newPage) {
                         removeButtonsLi.appendChild(removeButton);
                     }
 
-                    let replaceArr = GM_getValue('replace'),
+                    const replaceArr = GM_getValue('replace'),
                         replaceButtonsLi = c('li');
-                    replaceButtonsLi.appendChild(hr());
+                    replaceButtonsLi.appendChild(c('hr'));
                     for (let i = 0; i < replaceArr.length; i++) {
-                        let replaceButton = c('button');
+                        const replaceButton = c('button');
                         replaceButton.innerHTML = 'Stop renaming "' + replaceArr[i][0] + '" to "' + replaceArr[i][1] + '"';
                         replaceButton.addEventListener('click', removeReplacer);
                         replaceButton.dataset.id = replaceArr[i][0];
@@ -378,7 +369,7 @@ function setup(newPage) {
 }
 
 function removeRemover(e) {
-    let removeArr = GM_getValue('remove'),
+    const removeArr = GM_getValue('remove'),
         pos = removeArr.indexOf(e.target.dataset.id);
     removeArr.splice(pos, 1);
     GM_setValue('remove', removeArr);
@@ -386,8 +377,8 @@ function removeRemover(e) {
 }
 
 function removeReplacer(e) {
-    let replaceArr = GM_getValue('replace'),
-        pos;
+    const replaceArr = GM_getValue('replace');
+    let pos;
     for (let i = 0; i < replaceArr.length; i++) {
         if (replaceArr[i][0] === e.target.dataset.id) {
             pos = i;
@@ -399,17 +390,17 @@ function removeReplacer(e) {
 }
 
 function addRemover() {
-    let remove = document.getElementById('selectRemove').value,
+    const remove = document.getElementById('selectRemove').value,
         x = document.getElementsByClassName('type_system depth_2 contains_branch')[0];
     if (remove != 'Pick element to be removed' && (x.querySelector(`a[title="${remove}"]`) || x.querySelector(`a[title="${remove} "]`))) {
-        let replaceArr = GM_getValue('replace');
+        const replaceArr = GM_getValue('replace');
         for (let i = 0; i < replaceArr.length; i++) {
             if (replaceArr[i][0].trim() == remove.trim()) {
                 alert('Error: Already used');
                 return;
             }
         }
-        let removeArr = GM_getValue('remove');
+        const removeArr = GM_getValue('remove');
         removeArr.push(remove);
         removeArr.sort();
         GM_setValue('remove', removeArr);
@@ -420,13 +411,13 @@ function addRemover() {
 }
 
 function addReplacer() {
-    let replace = document.getElementById('selectReplace').value,
+    const replace = document.getElementById('selectReplace').value,
         replaceWith = document.getElementById('inputReplace').value,
         x = document.getElementsByClassName('type_system depth_2 contains_branch')[0];
-    if (replace != 'Pick element to be replaced' && (x.querySelector(`a[title="${remove}"]`) || x.querySelector(`a[title="${remove} "]`))) {
+    if (replace != 'Pick element to be replaced' && (x.querySelector(`a[title="${replace}"]`) || x.querySelector(`a[title="${replace} "]`))) {
 
         if (replaceWith != '') {
-            let replaceArr = GM_getValue('replace');
+            const replaceArr = GM_getValue('replace');
 
             replaceArr.push([replace, replaceWith]);
             replaceArr.sort((a, b) => {
