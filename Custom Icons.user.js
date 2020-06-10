46313dbaa5fd775fb920a19e2de498403e793779
melusc <lusc1mc@gmail.com>
2020-06-10 18:29:26 +0200

    :fire:

--- Custom Icons.user.js
+++ Custom Icons.user.js
@@ -1,6 +1,6 @@
 // ===UserScript===
 // @name        Moodle Custom Icons
-// @version     2020.06.10b
+// @version     2020.06.10c
 // @include     *://moodle.ksasz.ch/*
 // @exclude     *://moodle.ksasz.ch/info*
 // @exclude     *://moodle.ksasz.ch/cleanMoodle*
@@ -17,7 +17,7 @@
 // @run-at      document-body
 // ===/UserScript===
 /* jshint esversion: 10 */
-/* Check out moodle preferences or https://moodle.ksasz.ch/customIcons/ directly */
+/* Check out preferences or https://moodle.ksasz.ch/customIcons/ directly */
 'use strict';
 
 addEventListener('customIcons', async() => {
@@ -27,7 +27,7 @@ addEventListener('customIcons', async() => {
         if (title === 'updatedAt') continue;
         try {
             const element = sidebar.querySelector(`a[title="${title}"]`),
-                  img = new Image();
+                img = new Image();
             img.classList.add('icon', 'fa', 'fa-fw', 'navicon');
             img.setAttribute('aria-hidden', 'true');
             img.setAttribute('tabindex', -1);
@@ -48,9 +48,9 @@ GM_addValueChangeListener('updatedAt', async(a, b, c, remote) => {
         await fetch('/')
             .then(e => e.text())
             .then(e => {
-            e = new DOMParser().parseFromString(e, 'text/html');
-            document.getElementById('inst4').innerHTML = e.getElementById('inst4').innerHTML;
-        });
+                e = new DOMParser().parseFromString(e, 'text/html');
+                document.getElementById('inst4').innerHTML = e.getElementById('inst4').innerHTML;
+            });
         dispatchEvent(new Event('cleanMoodle'));
         dispatchEvent(new Event('customIcons'));
         dispatchEvent(new Event('moreSidebarLinks'));
@@ -60,8 +60,9 @@ GM_addValueChangeListener('updatedAt', async(a, b, c, remote) => {
 addEventListener('DOMContentLoaded', () => {
 
     if (location.pathname === '/user/preferences.php') {
+        dispatchEvent(new Event('customIcons'));
         const row = document.getElementById('maincontent').parentElement.children[2],
-              block = row.children[0].cloneNode(true);
+            block = row.children[0].cloneNode(true);
         block.getElementsByClassName('card-title')[0].innerText = 'Custom Icons';
         row.appendChild(block);
         const cardText = block.getElementsByClassName('card-text')[0];
@@ -71,7 +72,7 @@ addEventListener('DOMContentLoaded', () => {
         block.getElementsByTagName('a')[0].href = '/customIcons/';
         block.getElementsByTagName('a')[0].target = '_blank';
         block.getElementsByTagName('a')[0].innerText = 'Settings';
-    } else if ( !location.pathname.toLowerCase().startsWith('/customicons') ) {
+    } else if (!location.pathname.toLowerCase().startsWith('/customicons')) {
         dispatchEvent(new Event('customIcons'));
     }
 });
@@ -92,137 +93,137 @@ if (location.pathname.toLowerCase().startsWith('/customicons')) {
     fetch('/')
         .then(e => e.text())
         .then(e => {
-        const parsed = new DOMParser().parseFromString(e, 'text/html');
+            const parsed = new DOMParser().parseFromString(e, 'text/html');
 
-        let element = parsed.getElementById('inst4'),
-            tree = parsed.getElementById('inst4').cloneNode(true);
-        while (element.parentElement.nodeName !== 'BODY') {
-            let temp = tree;
-            element = element.parentElement;
-            tree = element.cloneNode(false);
-            tree.appendChild(temp);
-        }
-        document.body.appendChild(tree);
+            let element = parsed.getElementById('inst4'),
+                tree = parsed.getElementById('inst4').cloneNode(true);
+            while (element.parentElement.nodeName !== 'BODY') {
+                let temp = tree;
+                element = element.parentElement;
+                tree = element.cloneNode(false);
+                tree.appendChild(temp);
+            }
+            document.body.appendChild(tree);
 
-        document.body.getElementsByClassName('type_setting depth_2 item_with_icon')[0].remove();
+            document.body.getElementsByClassName('type_setting depth_2 item_with_icon')[0].remove();
 
-        const regionMainBox = parsed.getElementById('region-main-box'),
-              regionMainBoxLi = regionMainBox.getElementsByClassName('section img-text')[0];
-
-
-        while (regionMainBoxLi.lastChild) regionMainBoxLi.lastChild.remove();
-        document.getElementById('page-content').insertBefore(regionMainBox, document.getElementById('page-content').children[0]);
-        document.getElementById('user-notifications').remove();
-        dispatchEvent(new Event('cleanMoodle'));
-        dispatchEvent(new Event('customIcons'));
-
-        const anchors = document.getElementsByTagName('a');
-        for (let i = 0; i < anchors.length; i++) {
-            anchors[i].onclick = addCourse;
-            anchors[i].removeAttribute('href');
-        }
+            const regionMainBox = parsed.getElementById('region-main-box'),
+                regionMainBoxLi = regionMainBox.getElementsByClassName('section img-text')[0];
 
 
-        regionMainBoxLi.style.listStyle = 'none';
-        const li = document.createElement('li'),
-              div = document.createElement('div'),
-              title = document.createElement('h2'),
-              form = document.createElement('form');
-        title.innerText = 'Add icon';
-        form.id = 'form';
-        div.classList.add('mod-indent-outer', 'contentwithoutlink', 'no-overflow');
+            while (regionMainBoxLi.lastChild) regionMainBoxLi.lastChild.remove();
+            document.getElementById('page-content').insertBefore(regionMainBox, document.getElementById('page-content').children[0]);
+            document.getElementById('user-notifications').remove();
+            dispatchEvent(new Event('cleanMoodle'));
+            dispatchEvent(new Event('customIcons'));
 
-        li.appendChild(title);
-        li.appendChild(div);
-        div.appendChild(form);
-        regionMainBoxLi.appendChild(li);
-
-        const selectedCourseDiv = document.createElement('div'),
-              selectedCourseText = document.createTextNode('Select course on the left');
-        selectedCourseDiv.style.listStyle = 'none';
-        selectedCourseDiv.style.marginBottom = '25px';
-        selectedCourseDiv.dataset.selected = 'none';
-        selectedCourseDiv.id = 'selectedCourseDiv';
-        selectedCourseDiv.appendChild(selectedCourseText);
-        form.appendChild(selectedCourseDiv);
+            const anchors = document.getElementsByTagName('a');
+            for (let i = 0; i < anchors.length; i++) {
+                anchors[i].onclick = addCourse;
+                anchors[i].removeAttribute('href');
+            }
 
 
-        const fileInput = document.createElement('input'),
-              fileLabel = document.createElement('label');
-        fileInput.type = 'file';
-        fileInput.id = 'fileInput';
-        fileInput.accept = '.png,.jpg,.jpeg';
-        fileInput.onchange = handleChange;
-        fileLabel.for = 'fileInput';
-        fileLabel.innerText = 'Submit file:';
-        fileLabel.style.display = 'block';
-        fileLabel.style.marginTop = '5px';
-        form.appendChild(fileLabel);
-        form.appendChild(fileInput);
+            regionMainBoxLi.style.listStyle = 'none';
+            const li = document.createElement('li'),
+                div = document.createElement('div'),
+                title = document.createElement('h2'),
+                form = document.createElement('form');
+            title.innerText = 'Add icon';
+            form.id = 'form';
+            div.classList.add('mod-indent-outer', 'contentwithoutlink', 'no-overflow');
 
-        const resetButton = document.createElement('button');
-        resetButton.innerText = 'Reset File';
-        resetButton.id = 'resetButton';
-        resetButton.onclick = resetFile;
-        resetButton.style.display = 'block';
-        resetButton.style.marginTop = '5px';
-        resetButton.style.marginBottom = '25px';
-        form.appendChild(resetButton);
+            li.appendChild(title);
+            li.appendChild(div);
+            div.appendChild(form);
+            regionMainBoxLi.appendChild(li);
 
-
-        const urlInput = document.createElement('input'),
-              urlLabel = document.createElement('label');
-        urlInput.type = 'url';
-        urlInput.id = 'urlInput';
-        urlInput.placeholder = 'https://i.imgur.com/...';
-        urlInput.onchange = handleChange;
-        urlInput.onkeyup = handleChange;
-        urlInput.onkeydown = handleFile;
-        urlLabel.for = 'urlInput';
-        urlLabel.innerText = 'Upload from Url:';
-        urlLabel.style.display = 'block';
-        form.appendChild(urlLabel);
-        form.appendChild(urlInput);
+            const selectedCourseDiv = document.createElement('div'),
+                selectedCourseText = document.createTextNode('Select course on the left');
+            selectedCourseDiv.style.listStyle = 'none';
+            selectedCourseDiv.style.marginBottom = '25px';
+            selectedCourseDiv.dataset.selected = 'none';
+            selectedCourseDiv.id = 'selectedCourseDiv';
+            selectedCourseDiv.appendChild(selectedCourseText);
+            form.appendChild(selectedCourseDiv);
 
 
-        const submit = document.createElement('button');
-        submit.innerText = 'Submit';
-        submit.onclick = handleFile;
-        submit.style.display = 'block';
-        submit.style.marginTop = '35px';
-        form.appendChild(submit);
+            const fileInput = document.createElement('input'),
+                fileLabel = document.createElement('label');
+            fileInput.type = 'file';
+            fileInput.id = 'fileInput';
+            fileInput.accept = '.png,.jpg,.jpeg';
+            fileInput.onchange = handleChange;
+            fileLabel.for = 'fileInput';
+            fileLabel.innerText = 'Submit file:';
+            fileLabel.style.display = 'block';
+            fileLabel.style.marginTop = '5px';
+            form.appendChild(fileLabel);
+            form.appendChild(fileInput);
+
+            const resetButton = document.createElement('button');
+            resetButton.innerText = 'Reset File';
+            resetButton.id = 'resetButton';
+            resetButton.onclick = resetFile;
+            resetButton.style.display = 'block';
+            resetButton.style.marginTop = '5px';
+            resetButton.style.marginBottom = '25px';
+            form.appendChild(resetButton);
 
 
-        const list = document.createElement('li'),
-              divList = document.createElement('div'),
-              titleList = document.createElement('h2'),
-              unsortedList = document.createElement('div');
-        unsortedList.id = 'buttonsDiv';
-        unsortedList.style.listStyle = 'none';
-        titleList.innerText = 'Remove icon';
-        titleList.style.marginTop = '25px';
-        divList.classList.add('mod-indent-outer', 'contentwithoutlink', 'no-overflow');
-        divList.appendChild(unsortedList);
-        list.appendChild(titleList);
-        list.appendChild(divList);
-        regionMainBoxLi.appendChild(list);
+            const urlInput = document.createElement('input'),
+                urlLabel = document.createElement('label');
+            urlInput.type = 'url';
+            urlInput.id = 'urlInput';
+            urlInput.placeholder = 'https://i.imgur.com/...';
+            urlInput.onchange = handleChange;
+            urlInput.onkeyup = handleChange;
+            urlInput.onkeydown = handleFile;
+            urlLabel.for = 'urlInput';
+            urlLabel.innerText = 'Upload from Url:';
+            urlLabel.style.display = 'block';
+            form.appendChild(urlLabel);
+            form.appendChild(urlInput);
 
-        appendButtons(unsortedList);
-    });
+
+            const submit = document.createElement('button');
+            submit.innerText = 'Submit';
+            submit.onclick = handleFile;
+            submit.style.display = 'block';
+            submit.style.marginTop = '35px';
+            form.appendChild(submit);
+
+
+            const list = document.createElement('li'),
+                divList = document.createElement('div'),
+                titleList = document.createElement('h2'),
+                unsortedList = document.createElement('div');
+            unsortedList.id = 'buttonsDiv';
+            unsortedList.style.listStyle = 'none';
+            titleList.innerText = 'Remove icon';
+            titleList.style.marginTop = '25px';
+            divList.classList.add('mod-indent-outer', 'contentwithoutlink', 'no-overflow');
+            divList.appendChild(unsortedList);
+            list.appendChild(titleList);
+            list.appendChild(divList);
+            regionMainBoxLi.appendChild(list);
+
+            appendButtons(unsortedList);
+        });
 
 }
 
 function fetcher(src) {
     return new Promise(resolve => {
         const value = GM_getValue(src),
-              response = atob(value.split('base64,')[1]),
-              byteNumbers = response.split('').map(e => e.charCodeAt(0)),
-              byteArray = new Uint8Array(byteNumbers),
-              type = value.split(';')[0].split(':')[1],
-              blob = new Blob([byteArray], {
-                  type: type
-              }),
-              objectUrl = URL.createObjectURL(blob);
+            response = atob(value.split('base64,')[1]),
+            byteNumbers = response.split('').map(e => e.charCodeAt(0)),
+            byteArray = new Uint8Array(byteNumbers),
+            type = value.split(';')[0].split(':')[1],
+            blob = new Blob([byteArray], {
+                type: type
+            }),
+            objectUrl = URL.createObjectURL(blob);
 
         resolve(objectUrl);
     });
@@ -230,8 +231,8 @@ function fetcher(src) {
 
 function handleChange() {
     const fileInput = document.getElementById('fileInput'),
-          urlInput = document.getElementById('urlInput'),
-          fileReset = document.getElementById('resetButton');
+        urlInput = document.getElementById('urlInput'),
+        fileReset = document.getElementById('resetButton');
     if (fileInput.files[0]) {
         if (/(\.jpe?g|\.png)$/i.test(fileInput.files[0].name)) {
             urlInput.disabled = true;
@@ -256,8 +257,8 @@ async function addCourse(e) {
     e = e.target.closest('p');
 
     const selectedCourseDiv = document.getElementById('selectedCourseDiv'),
-          img = e.getElementsByTagName('img')[0],
-          anchor = e.getElementsByTagName('a')[0];
+        img = e.getElementsByTagName('img')[0],
+        anchor = e.getElementsByTagName('a')[0];
 
     selectedCourseDiv.lastChild.remove();
     if (img && new URL(img.src).protocol === 'blob:') {
@@ -278,7 +279,7 @@ function resetFile(e) {
         e.stopPropagation();
     }
     const form = document.createElement('form'),
-          fileInput = document.getElementById('fileInput');
+        fileInput = document.getElementById('fileInput');
     fileInput.after(form);
     form.appendChild(fileInput);
     form.reset();
@@ -288,16 +289,16 @@ function resetFile(e) {
 }
 
 async function handleFile(e) {
-    if (e.key === 'Enter' || e.key === undefined){
+    if (e.key === 'Enter' || e.key === undefined) {
         e.preventDefault();
         e.stopPropagation();
 
         const fileInput = document.getElementById('fileInput'),
-              urlInput = document.getElementById('urlInput'),
-              resetButton = document.getElementById('resetButton'),
-              selectedDiv = document.getElementById('selectedCourseDiv'),
-              form = document.getElementById('form'),
-              selectedCourseText = document.createTextNode('Select course on the left');
+            urlInput = document.getElementById('urlInput'),
+            resetButton = document.getElementById('resetButton'),
+            selectedDiv = document.getElementById('selectedCourseDiv'),
+            form = document.getElementById('form'),
+            selectedCourseText = document.createTextNode('Select course on the left');
 
         if (!((fileInput.files[0] || urlInput.value) && selectedDiv.dataset.selected !== 'none')) {
             alert('Error: Empty or falsy input');
@@ -312,8 +313,8 @@ async function handleFile(e) {
                     fetch(url)
                         .then(e => e.blob())
                         .then(e => {
-                        resolve(e);
-                    });
+                            resolve(e);
+                        });
                 } catch (a) {
                     alert('Error: empty input');
                     console.error(a);
@@ -350,9 +351,9 @@ async function removeIcon(e) {
     await fetch('/')
         .then(e => e.text())
         .then(e => {
-        e = new DOMParser().parseFromString(e, 'text/html');
-        document.getElementById('inst4').innerHTML = e.getElementById('inst4').innerHTML;
-    });
+            e = new DOMParser().parseFromString(e, 'text/html');
+            document.getElementById('inst4').innerHTML = e.getElementById('inst4').innerHTML;
+        });
     const anchors = document.getElementsByTagName('a');
     for (let i = 0; i < anchors.length; i++) {
         anchors[i].onclick = addCourse;
