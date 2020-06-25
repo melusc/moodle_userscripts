7d7e4aec51732ce41fce2e2ba03c98544d88e92c
melusc <lusc1mc@gmail.com>
2020-06-25 12:16:12 +0200

    :fire:

--- docs/htmlKeyboard.js
+++ docs/htmlKeyboard.js
@@ -1,66 +1,49 @@
-var shift = false,
-    capslock = false,
-    style = document.createElement('style');
+let shift = false,
+    capslock = false;
+const style = document.createElement('style'),
+    p1 = document.getElementById("p1");
 style.innerHTML = 'button:not(.not){text-transform:uppercase}';
+document.getElementById('table').onclick = addTextNode;
 
 function upperCase() {
     if (shift ^ capslock) {
-        document.head.appendChild(style)
+        document.head.appendChild(style);
     } else {
-        document.head.removeChild(style)
+        document.head.removeChild(style);
     }
 }
 
-function addTextNode(text) {
-    text = text.innerHTML.toLowerCase();
-    console.log(text);
-    var p1 = document.getElementById("p1"),
-        br = document.createElement('br');
-    if (text == '[enter]') {
-        p1.appendChild(br)
-    } else if (text == '[backspace]') {
-        deleter()
-    } else if (text == '[capslk]') {
-        capsLock()
-    } else if (text == '[shift]') {
-        shifter()
-    } else {
-        if (shift ^ capslock) {
-            text = text.toUpperCase()
+function addTextNode(e) {
+    if (e.target.nodeName === 'BUTTON') {
+        let text = e.target.textContent.toLowerCase();
+        switch (text) {
+            case '[enter]':
+                const br = document.createElement('br');
+                p1.appendChild(br);
+                break;
+            case '[backspace]':
+                p1.innerText = p1.innerText.slice(0,p1.innerText.length - 1);
+                break;
+            case '[capslk]':
+                capslock = !capslock;
+                upperCase();
+                break;
+            case '[shift]':
+                shift = !shift;
+                upperCase();
+                break;
+            case '[space]':
+                p1.innerText = p1.innerText + ' ';
+                break;
+            default:
+                if (shift ^ capslock) {
+                    text = text.toUpperCase();
+                }
+                if (shift) {
+                    shift = false;
+                    upperCase();
+                }
+                p1.innerText = p1.innerText + text;
         }
-        if (shift) {
-            shift = false;
-            upperCase()
-        }
-        if (text.toLowerCase() == '[space]') {
-            text = ' '
-        }
-        let newtext = document.createTextNode(text);
-        p1.appendChild(newtext)
     }
-}
-
-function shifter() {
-    if (!shift) {
-        shift = true;
-        upperCase()
-    } else {
-        shift = false;
-        upperCase()
-    }
-}
-
-function capsLock() {
-    if (capslock) {
-        capslock = false;
-        upperCase()
-    } else {
-        capslock = true;
-        upperCase()
-    }
-}
-
-function deleter() {
-    var p1 = document.getElementById('p1');
-    p1.innerHTML = p1.innerHTML.slice(0, p1.innerHTML.length - 1)
 }
