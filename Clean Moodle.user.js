00828155294aaff8e1e123afcf33c6eebda6f465
melusc <lusc1mc@gmail.com>
2020-07-29 17:52:14 +0200

    :fire:

--- Clean Moodle.user.js
+++ Clean Moodle.user.js
@@ -1,7 +1,7 @@
 // ===UserScript===
 // @name         Clean Moodle
 // @namespace    https://github.com/melusc/lusc
-// @version      2020.07.28b
+// @version      2020.07.29a
 // @include      *://moodle.ksasz.ch/*
 // @exclude      *://moodle.ksasz.ch/info*
 // @exclude      *://moodle.ksasz.ch/lib*
