96e3e89af809b662af9ca9f284359fe1342f99a1
melusc <lusc1mc@gmail.com>
2021-01-16 09:25:16 +0100

    Add support for jsx with eslint-config-preact and @babel/plugin-transform-react-jsx

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,11 @@
-/*
-  globals
-  module: false
-*/
-
 module.exports = {
   plugins: [
     '@babel/plugin-syntax-class-properties',
     'htm',
+    [ '@babel/plugin-transform-react-jsx', {
+      pragma: 'h',
+      pragmaFrag: 'Fragment',
+    } ],
   ],
   /* generatorOpts: {
     minified: true,
