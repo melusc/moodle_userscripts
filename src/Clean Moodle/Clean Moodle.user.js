280fb8fad5b35b0671df3136b619e530ba4505bb
melusc <lusc1mc@gmail.com>
2021-01-21 19:38:18 +0100

    Clean Moodle: use shared login logic

--- src/Clean Moodle/Clean Moodle.user.js
+++ src/Clean Moodle/Clean Moodle.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Clean Moodle with Preact
-// @version      2021.01.21b
+// @version      2021.01.21c
 // @author       lusc
 // @include      *://moodle.ksasz.ch/*
 // @updateURL    https://github.com/melusc/moodle_userscripts/raw/master/dist/Clean%20Moodle/Clean%20Moodle.user.js
@@ -13,7 +13,7 @@
 // @run-at       document-start
 // ==/UserScript==
 
-import frontPageCss from './frontpage.scss';
+import { getCourses } from '../shared/moodle-functions';
 import settingsPageCss from './settingspage.scss';
 
 import { render, Component, Fragment, h } from 'preact';
@@ -22,7 +22,9 @@ if ( location.protocol !== 'https:' ) {
   location.protocol = 'https:';
 }
 
-const runOnlyOnceOnFrontpage = () => {
+const isFrontpage = !( /^\/cleanmoodlepreact/iu ).test( location.pathname );
+
+const runOnceOnFrontpage = () => {
   const sidebar = getSidebar( document );
 
   GM_registerMenuCommand(
@@ -347,7 +349,10 @@ const refresh = ( () => {
         // removing anchors leaves the sidebar still sorted
 
         if ( removedVals.length > 0 ) {
-          getCourses().then( coursesObj => {
+          getCourses(
+            false,
+            currentPageReturnState
+          ).then( coursesObj => {
             for ( const id of removedVals ) {
               const fullname = coursesObj[ id ];
 
@@ -398,297 +403,23 @@ const refresh = ( () => {
   };
 } )();
 
-const getCourses = ( () => {
-  let courses;
-
-  return ( noCache = false ) => {
-    if ( noCache || !courses ) {
-      courses = Promise.all( [ login(), getUserId() ] )
-        .then( ( [ token, userid ] ) => {
-          const bodyParams = new URLSearchParams();
-
-          bodyParams.set(
-            'requests[0][function]',
-            'core_enrol_get_users_courses'
-          );
-          bodyParams.set(
-            'requests[0][arguments]',
-            JSON.stringify( {
-              userid,
-              returnusercount: false,
-            } )
-          );
-          bodyParams.set(
-            'wsfunction',
-            'tool_mobile_call_external_functions'
-          );
-          bodyParams.set(
-            'wstoken',
-            token
-          );
-
-          return fetch(
-            '/webservice/rest/server.php?moodlewsrestformat=json',
-            {
-              method: 'POST',
-              body: bodyParams.toString(),
-              headers: {
-                'content-type': 'application/x-www-form-urlencoded',
-              },
-            }
-          ).then( e => e.json() );
-        } )
-        .then( responseJSON => {
-          if ( responseJSON.hasOwnProperty( 'exception' ) ) {
-            logout();
-            return getCourses( true );
-          }
-
-          const data = JSON.parse( responseJSON.responses[ 0 ].data );
-
-          const coursesObj = {};
-
-          for ( const { id, fullname } of data ) {
-            coursesObj[ id ] = fullname;
-          }
-
-          setLastValidatedToken();
-
-          return coursesObj;
-        } );
-    }
-
-    return courses;
-  };
-} )();
-
-const getUserId = () => login()
-  .then( token => {
-    const bodyParams = new URLSearchParams();
-
-    bodyParams.set(
-      'wsfunction',
-      'core_webservice_get_site_info'
-    );
-    bodyParams.set(
-      'wstoken',
-      token
-    );
-
-    return fetch(
-      '/webservice/rest/server.php?moodlewsrestformat=json',
-      {
-        method: 'POST',
-        headers: {
-          'content-type': 'application/x-www-form-urlencoded',
-        },
-        body: bodyParams.toString(),
-      }
-    );
-  } )
-  .then( res => res.json() )
-  .then( responseJSON => {
-    if ( responseJSON.hasOwnProperty( 'exception' ) ) {
-      logout();
-      return getUserId();
-    }
-
-    setLastValidatedToken();
-
-    return responseJSON.userid;
-  } );
-
-const setLastValidatedToken = () => GM_setValue(
-  'lastValidatedToken',
-  new Date().getTime()
-);
-
-const login = ( () => {
-  let cachedToken;
-
-  return ( noCache = false ) => {
-    if ( cachedToken ) {
-      return cachedToken;
-    }
-
-    const storedToken = GM_getValue( 'token' );
-    const lastValidated = GM_getValue( 'lastValidatedToken' );
-    if (
-      !cachedToken
-      && storedToken
-      && new Date().getTime() - lastValidated < 18000000
-    ) {
-      // less than 5h
-      cachedToken = Promise.resolve( storedToken ); // to make it a Promise and as such "thenable"
-    }
-
-    if ( noCache || !cachedToken ) {
-      cachedToken = getCredentials().then( ( { username, password } ) => {
-        const loginParams = new URLSearchParams();
-
-        loginParams.set(
-          'username',
-          username
-        );
-        loginParams.set(
-          'password',
-          password
-        );
-        loginParams.set(
-          'service',
-          'moodle_mobile_app'
-        );
-        return fetch(
-          '/login/token.php',
-          {
-            method: 'POST',
-            body: loginParams.toString(),
-            headers: {
-              'content-type': 'application/x-www-form-urlencoded',
-            },
-          }
-        )
-          .then( e => e.json() )
-          .then( response => {
-            if ( response.hasOwnProperty( 'errorcode' ) ) {
-              logout( true );
-              return login( true );
-            }
-
-            GM_setValue(
-              'token',
-              response.token
-            );
-            setLastValidatedToken();
-
-            return response.token;
-          } );
-      } );
-    }
-
-    return cachedToken;
-  };
-} )();
-
-const getCredentials = () => new Promise( resolve => {
-  const callback = ( { username, password } ) => {
-    GM_setValue(
-      'username',
-      username
-    );
-    GM_setValue(
-      'password',
-      password
-    );
-    resolve( { username, password } );
-  };
-
-  const username = GM_getValue( 'username' );
-  const password = GM_getValue( 'password' );
-  if ( username && password ) {
-    resolve( { username, password } );
-  }
-  else {
-    const state = {
-      loggedOut: true,
-      loggedOutCallback: callback,
-    };
-
-    if ( isFrontpage ) {
-      if ( typeof frontPageLoginSetState === 'function' ) {
-        frontPageLoginSetState( state );
-      }
-      else {
-        frontPageDefaultLoginState = state;
-        const div = document.createElement( 'div' );
-        div.className = 'clean-moodle';
-        document.body.append( div );
-        GM_addStyle( frontPageCss );
-        render(
-          <FrontPageLogin />,
-          div
-        );
-      }
-    }
-    else {
-      settingsPageSetState( state );
-    }
-  }
-} );
-
-let frontPageLoginSetState;
-let frontPageDefaultLoginState = {};
-
-class FrontPageLogin extends Component {
-  state = frontPageDefaultLoginState;
-
-  inputs = {};
-
-  render = (
-    _props, { loggedOut }
-  ) => loggedOut
-      && <div class="vertical-horizontal-center">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">Clean Moodle Login</h5>
-            <input
-              placeholder="Username"
-              required
-              class="input-group-text"
-              ref={e => {
-                this.inputs.username = e;
-              }}
-            />
-            <input
-              placeholder="Password"
-              required
-              class="input-group-text"
-              ref={e => {
-                this.inputs.password = e;
-              }}
-              type="password"
-            />
-          </div>
-          <button class="btn btn-primary" onClick={this.handleClick}>
-            Login
-          </button>
-        </div>
-      </div>
-    ;
-
-  handleClick = () => {
-    const username = this.inputs.username.value.trim();
-    const password = this.inputs.password.value;
-
-    if ( username && password ) {
-      this.setState( { loggedOut: false } );
-      this.state.loggedOutCallback( {
-        username,
-        password,
-      } );
-    }
-  };
-
-  componentDidMount = () => {
-    frontPageLoginSetState = state => {
-      this.setState( state );
-    };
-  };
-}
-
-const logout = ( removeCredentials = false ) => {
-  [ 'token', 'lastValidatedToken' ].map( GM_deleteValue );
-  if ( removeCredentials ) {
-    [ 'username', 'password' ].map( GM_deleteValue );
-  }
+const settingsPageReturnState = state => {
+  settingsPageSetState( state );
 };
+
+const currentPageReturnState = isFrontpage
+  ? undefined
+  : settingsPageReturnState;
 
 const getElem = (
   id, sidebar
 ) => sidebar.querySelector( `a[href="https://moodle.ksasz.ch/course/view.php?id=${ id }"]` );
 
 const testForInexistantCourse = id => {
-  getCourses().then( courses => {
+  getCourses(
+    false,
+    currentPageReturnState
+  ).then( courses => {
     if ( !courses.hasOwnProperty( id ) ) {
       removeElementFromStorage( id );
       alert( `You appear to not be in the course with the id "${ id }" anymore.\nThe course will not be checked for anymore` );
@@ -806,7 +537,6 @@ class Main extends Component {
     return (
       <div class="outerMain">
         <div class="main">
-
           {loggedOut
             ? <>
               <div class="replace-flex-input">
@@ -829,6 +559,7 @@ class Main extends Component {
                 </button>
               </div>
             </>
+
             : <>
               <div class="section-title">Rename course</div>
               <div class="replace-flex-inputs">
@@ -1053,7 +784,10 @@ class SettingsPage extends Component {
       this.setState( state );
     };
 
-    getCourses().then( coursesObj => {
+    getCourses(
+      false,
+      currentPageReturnState
+    ).then( coursesObj => {
       const courses = Object.entries( coursesObj ).map( ( [ id, fullname ] ) => ( {
         id,
         name: fullname,
@@ -1196,12 +930,10 @@ const getSidebar = context => context.querySelector( 'li[aria-labelledby="label_
   }
 }
 
-let isFrontpage = true;
-
 if ( !( /^\/customicons/iu ).test( location.pathname ) ) {
-  const functionToRun = ( /^\/cleanmoodlepreact/iu ).test( location.pathname )
-    ? ( isFrontpage = false, initSettingsPage )
-    : runOnlyOnceOnFrontpage;
+  const functionToRun = isFrontpage
+    ? runOnceOnFrontpage
+    : initSettingsPage;
 
   document.readyState === 'complete'
     ? functionToRun()
