7c0870a165442de8a90ef0843b136689c29b27f9
melusc <lusc1mc@gmail.com>
2021-01-22 16:31:23 +0100

    Download Courses' Content: Move to shared login logic

--- src/Download Courses Content/Moodle Download Courses Content.user.js
+++ src/Download Courses Content/Moodle Download Courses Content.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Moodle Download Course's Content
-// @version      2021.01.21a
+// @version      2021.01.22a
 // @author       lusc
 // @include      https://moodle.ksasz.ch/course/view.php?id=*
 // @updateURL    https://github.com/melusc/moodle_userscripts/raw/master/dist/Download%20Courses%20Content/Moodle%20Download%20Courses%20Content.user.js
@@ -13,10 +13,11 @@
 // @connect      *
 // ==/UserScript==
 
-import { render, h, Component } from 'preact';
+window.nonExistantFunc?.(); //  for whatever reason, without this, webpack optimises the metadata above away
+
 import saveAs from 'file-saver';
 import JSZip from 'jszip/dist/jszip';
-import style from './style.scss';
+import { login, logout, setLastValidatedToken } from '../shared/moodle-functions';
 
 const init = () => {
   if ( !document.querySelector( '#region-main div.errorbox.alert.alert-danger' ) ) {
@@ -224,237 +225,6 @@ const initDownload = (
       } );
   } );
 };
-
-const setLastValidatedToken = () => GM_setValue(
-  'lastValidatedToken',
-  new Date().getTime()
-);
-
-const logout = ( removeCredentials = false ) => {
-  [ 'token', 'lastValidatedToken' ].forEach( GM_deleteValue );
-  if ( removeCredentials ) {
-    [ 'username', 'password' ].forEach( GM_deleteValue );
-  }
-};
-
-const login = ( () => {
-  let cachedToken;
-
-  return ( noCache = false ) => {
-    if ( cachedToken && !noCache ) {
-      return cachedToken;
-    }
-
-    const storedToken = GM_getValue( 'token' );
-    const lastValidated = GM_getValue( 'lastValidatedToken' );
-    if (
-      !cachedToken
-      && storedToken
-      && new Date().getTime() - lastValidated < 18000000
-    ) {
-      // less than 5h
-      cachedToken = Promise.resolve( storedToken ); // to make it thenable
-    }
-
-    if ( noCache || !cachedToken ) {
-      cachedToken = getCredentials().then( ( { username, password } ) => {
-        const loginParams = new URLSearchParams();
-
-        loginParams.set(
-          'username',
-          username
-        );
-        loginParams.set(
-          'password',
-          password
-        );
-        loginParams.set(
-          'service',
-          'moodle_mobile_app'
-        );
-        return fetch(
-          '/login/token.php',
-          {
-            method: 'POST',
-            body: loginParams.toString(),
-            headers: {
-              'content-type': 'application/x-www-form-urlencoded',
-            },
-          }
-        )
-          .then( e => e.json() )
-          .then( response => {
-            if ( response.hasOwnProperty( 'errorcode' ) ) {
-              logout( true );
-              return login( true );
-            }
-
-            GM_setValue(
-              'token',
-              response.token
-            );
-            setLastValidatedToken();
-
-            return response.token;
-          } );
-      } );
-    }
-
-    return cachedToken;
-  };
-} )();
-
-const getCredentials = () => new Promise( resolve => {
-  const username = GM_getValue( 'username' );
-  const password = GM_getValue( 'password' );
-  if ( username && password ) {
-    resolve( { username, password } );
-  }
-  else {
-    const callback = ( { username, password } ) => {
-      GM_setValue(
-        'username',
-        username
-      );
-      GM_setValue(
-        'password',
-        password
-      );
-      loginPopupSetState( { isLoggedOut: false, loggedOutCallback: null } );
-      resolve( { username, password } );
-    };
-
-    const state = {
-      isLoggedOut: true,
-      loggedOutCallback: callback,
-      usernameIsInvalid: false,
-      passwordIsInvalid: false,
-    };
-    if ( typeof loginPopupSetState === 'function' ) {
-      loginPopupSetState( state );
-    }
-    else {
-      loginPopupDefaultState = state;
-      const div = document.createElement( 'div' );
-      div.className = 'dcc';
-      document.body.append( div );
-      render(
-        <LoginPopup />,
-        div
-      );
-
-      GM_addStyle( style );
-    }
-  }
-} );
-
-let loginPopupDefaultState;
-let loginPopupSetState;
-
-class LoginPopup extends Component {
-  state = loginPopupDefaultState;
-
-  componentDidMount() {
-    loginPopupSetState = state => {
-      this.setState( state );
-    };
-  }
-
-  inputs = {};
-
-  render = (
-    _props, { isLoggedOut, usernameIsInvalid, passwordIsInvalid }
-  ) => isLoggedOut
-      && <div class="vertical-horizontal-center">
-        <div class="card">
-          <div class="card-body">
-            <h5 class="card-title">Clean Moodle Login</h5>
-
-            <div>
-              <label for="dcc-password" class="form-label">
-                Username
-              </label>
-              <input
-                onInput={this.handleInput}
-                placeholder="Username"
-                required
-                id="dcc-username"
-                class={`input-group-text ${
-                  usernameIsInvalid
-                    ? 'is-invalid'
-                    : 'is-valid'
-                }`}
-                ref={e => {
-                  this.inputs.username = e;
-                }}
-              />
-              <div class="invalid-feedback">Please provide a username.</div>
-            </div>
-
-            <div>
-              <label for="dcc-password" class="form-label">
-                Password
-              </label>
-              <input
-                onInput={this.handleInput}
-                placeholder="Password"
-                required
-                id="dcc-password"
-                class={`input-group-text ${
-                  passwordIsInvalid
-                    ? 'is-invalid'
-                    : 'is-valid'
-                }`}
-                ref={e => {
-                  this.inputs.password = e;
-                }}
-                type="password"
-              />
-              <div class="invalid-feedback">Please provide a password.</div>
-            </div>
-          </div>
-          <button class="btn btn-primary" onClick={this.handleClick}>
-            Login
-          </button>
-        </div>
-      </div>
-    ;
-
-  handleInput = ( { target } ) => {
-    const { inputs } = this;
-    if ( target === inputs.username ) {
-      const username = inputs.username.value.trim();
-      this.setState( { usernameIsInvalid: !username } );
-    }
-    else {
-      const password = inputs.password.value;
-      this.setState( {
-        passwordIsInvalid: !password,
-      } );
-    }
-  };
-
-  handleClick = () => {
-    const username = this.inputs.username.value.trim();
-    const password = this.inputs.password.value;
-
-    if ( username && password ) {
-      this.setState( { loggedOut: false, loggedOutCallback: null } );
-      this.state.loggedOutCallback( {
-        username,
-        password,
-      } );
-    }
-    else {
-      if ( !username ) {
-        this.setState( { usernameIsInvalid: true } );
-      }
-      if ( !password ) {
-        this.setState( { passwordIsInvalid: true } );
-      }
-    }
-  };
-}
 
 document.readyState === 'complete'
   ? init()
