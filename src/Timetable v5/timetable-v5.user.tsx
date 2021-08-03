cbc2bc49b17b6cbbd7a49f4a8464d18cefcb0cef
Luca <61409445+melusc@users.noreply.github.com>
2021-08-03 14:34:15 +0200

    Bump version of timetable-v5

--- src/Timetable v5/timetable-v5.user.tsx
+++ src/Timetable v5/timetable-v5.user.tsx
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name      Moodle Timetable v5
-// @version   2021.07.27a
+// @version   2021.08.03a
 // @author    lusc
 // @updateURL https://git.io/Jqlt4
 // @include   *://moodle.ksasz.ch/
