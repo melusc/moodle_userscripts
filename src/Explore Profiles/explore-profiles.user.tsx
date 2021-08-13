ed70cbaa746ed5310c1e3597f254f9317f892b87
Luca <61409445+melusc@users.noreply.github.com>
2021-08-13 14:36:52 +0200

    Rebuild Explore Profiles after valtio update

--- src/Explore Profiles/explore-profiles.user.tsx
+++ src/Explore Profiles/explore-profiles.user.tsx
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name      Moodle explore profiles rest
-// @version   2021.08.03a
+// @version   2021.08.13a
 // @author    lusc
 // @updateURL https://git.io/JqltR
 // @include   https://moodle.ksasz.ch/user/profile.php?id=*
