a88f45948841bf726289daf2ab261e159851afcc
Luca <61409445+melusc@users.noreply.github.com>
2021-09-05 15:59:09 +0200

    Remove the deprecated and unused moodle-functions/

--- src/shared/moodle-functions/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './get-credentials';
-export * from './logout';
-export * from './login';
-export * from './set-last-validated-token';
-export * from './get-courses';
-export * from './get-user-id';
--- src/shared/moodle-functions/set-last-validated-token.ts
+++
@@ -1,3 +0,0 @@
-export const setLastValidatedToken = (): void => {
-	GM_setValue('lastValidatedToken', Date.now());
-};
