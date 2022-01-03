3002a2428b57cdffdaebc4a8f1b8eabc8c0b5680
Luca <61409445+melusc@users.noreply.github.com>
2022-01-03 13:30:58 +0100

    Allow for testing

--- production.config.js
+++ production.config.js
@@ -17,7 +17,7 @@ module.exports = (environment = {}) => ({
 		path.resolve(__dirname, 'src/**/*.user.{ts,tsx}'),
 	),
 	output: {
-		path: path.resolve(__dirname, 'dist'),
+		path: path.resolve(__dirname, 'dist/userscript-out'),
 		filename: '[name].user.js',
 		hashFunction: 'xxhash64',
 	},
