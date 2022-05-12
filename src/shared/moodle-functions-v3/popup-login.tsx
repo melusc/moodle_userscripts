c49de002f46f1d497346441c47e9553bb6680bd8
Luca <lusc.dev@gmail.com>
2022-05-12 21:27:21 +0200

    Use form for better submitting

--- src/shared/moodle-functions-v3/popup-login.tsx
+++ src/shared/moodle-functions-v3/popup-login.tsx
@@ -1,4 +1,4 @@
-import {render, h, FunctionalComponent} from 'preact';
+import {render, h, FunctionalComponent, JSX} from 'preact';
 import {useRef, useState} from 'preact/hooks';
 
 import genericPopupSCSS from './popup-login.scss';
@@ -19,7 +19,10 @@ const GenericPopup: FunctionalComponent<{
 		password: true,
 	});
 
-	const handleLogin = async () => {
+	const handleLogin: JSX.GenericEventHandler<HTMLFormElement> = async event => {
+		event.preventDefault();
+		event.stopImmediatePropagation();
+
 		const username = usernameRef.current?.value.trim();
 		const password = passwordRef.current?.value;
 
@@ -43,53 +46,59 @@ const GenericPopup: FunctionalComponent<{
 
 	return isLoggedOut ? (
 		<div class="vertical-horizontal-center">
-			<div class="card shadow">
-				<div class="card-body">
-					<h5 class="card-title">Login - {title}</h5>
-					<div class="mb-3">
-						<label htmlFor="popup-username" class="form-label">
-							Username
-						</label>
-						<input
-							ref={usernameRef}
-							required
-							defaultValue={getUsername()}
-							id="popup-username"
-							placeholder="Username"
-							class={`form-control${validities.username ? '' : ' is-invalid'}`}
-							onInput={() => {
-								setValidities(validities => ({
-									...validities,
-									username: true,
-								}));
-							}}
-						/>
-					</div>
+			<form onSubmit={handleLogin}>
+				<div class="card shadow">
+					<div class="card-body">
+						<h5 class="card-title">Login - {title}</h5>
+						<div class="mb-3">
+							<label htmlFor="popup-username" class="form-label">
+								Username
+							</label>
+							<input
+								ref={usernameRef}
+								required
+								defaultValue={getUsername()}
+								id="popup-username"
+								placeholder="Username"
+								class={`form-control${
+									validities.username ? '' : ' is-invalid'
+								}`}
+								onInput={() => {
+									setValidities(validities => ({
+										...validities,
+										username: true,
+									}));
+								}}
+							/>
+						</div>
 
-					<div class="mb-3">
-						<label htmlFor="popup-password" class="form-label">
-							Password
-						</label>
-						<input
-							ref={passwordRef}
-							required
-							id="popup-password"
-							placeholder="Password"
-							class={`form-control${validities.password ? '' : ' is-invalid'}`}
-							type="password"
-							onInput={() => {
-								setValidities(validities => ({
-									...validities,
-									password: true,
-								}));
-							}}
-						/>
+						<div class="mb-3">
+							<label htmlFor="popup-password" class="form-label">
+								Password
+							</label>
+							<input
+								ref={passwordRef}
+								required
+								id="popup-password"
+								placeholder="Password"
+								class={`form-control${
+									validities.password ? '' : ' is-invalid'
+								}`}
+								type="password"
+								onInput={() => {
+									setValidities(validities => ({
+										...validities,
+										password: true,
+									}));
+								}}
+							/>
+						</div>
 					</div>
+					<button class="btn btn-primary" type="submit">
+						Login
+					</button>
 				</div>
-				<button class="btn btn-primary" type="button" onClick={handleLogin}>
-					Login
-				</button>
-			</div>
+			</form>
 		</div>
 	) : null;
 };
