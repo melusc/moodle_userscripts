6db6eb4013072187affb61297cf422a49a914435
Luca <lusc.dev@gmail.com>
2022-05-13 13:57:50 +0200

    Remove v2

--- src/shared/moodle-functions-v2/logout.ts
+++
@@ -1,9 +0,0 @@
-export const logout = async (removeCredentials = false): Promise<void> => {
-	await GM.deleteValue('token');
-
-	if (removeCredentials) {
-		for (const key of ['username', 'password']) {
-			await GM.deleteValue(key);
-		}
-	}
-};
