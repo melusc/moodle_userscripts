7771c43d174921915f0bf690b689796bb12b2a59
Luca <61409445+melusc@users.noreply.github.com>
2022-09-21 16:29:30 +0200

    Add no-force-download (#520)

---
+++ src/no-force-download/no-force-download.user.ts
@@ -0,0 +1,26 @@
+// ==UserScript==
+// @name       No Force Download
+// @match      https://moodle.ksasz.ch/*
+// @grant      none
+// @version    1.0.0
+// @author     lusc
+// @license    MIT
+// @updateURL  https://github.com/melusc/moodle_userscripts/raw/userscript-out/no-force-download/no-force-download.user.js
+// @run-at     document-start
+// ==/UserScript==
+
+import {domReady} from '../shared/general-functions/dom-ready.js';
+
+domReady(() => {
+	const anchors = document.querySelectorAll<HTMLAnchorElement>(
+		'a[href*="?"][href*="forcedownload"]',
+	);
+
+	for (const anchor of anchors) {
+		if (anchor.search) {
+			const search = new URLSearchParams(anchor.search);
+			search.delete('forcedownload');
+			anchor.search = `?${search.toString()}`;
+		}
+	}
+});
