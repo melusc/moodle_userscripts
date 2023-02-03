307b6b307d5945acb667ed0809fbf9e4e3dca8db
melusc <melusc@users.noreply.github.com>
2023-02-03 07:50:43 +0000

    deploy: 13e9c18cf1debab1b3dc5b05c034c00c8af9e2bc

--- course-expand-all/course-expand-all.user.js
+++ course-expand-all/course-expand-all.user.js
@@ -6,9 +6,9 @@
 // @match       *://moodle*.*/course/view.php
 // @grant       none
 // @version     0.0.1
-// @author     lusc
-// @license    MIT
-// @run-at     document-start
-// @updateURL  https://github.com/melusc/moodle_userscripts/raw/userscript-out/course-expand-all/course-expand-all.user.js
+// @author      lusc
+// @license     MIT
+// @run-at      document-start
+// @updateURL   https://github.com/melusc/moodle_userscripts/raw/userscript-out/course-expand-all/course-expand-all.user.js
 // ==/UserScript==
 e=()=>{const e=document.querySelector('script[src$="/course/format/multitopic/format.js"]'),t=document.querySelector("div.collapsible-actions");if(!t||!e)return;const o=()=>{M.course.format.fmtCollapseAllOnClick({target:t,preventDefault(){}}),M.course.format.fmtCollapseOnHashChange=()=>{}};try{o()}catch{}e.addEventListener("load",o)},"interactive"!==document.readyState&&"complete"!==document.readyState?document.addEventListener("DOMContentLoaded",e,{once:!0}):e()})();
