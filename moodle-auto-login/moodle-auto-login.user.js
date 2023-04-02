a0922b3433471900802355027c3cc2118f445895
melusc <melusc@users.noreply.github.com>
2023-04-02 15:26:54 +0000

    deploy: 29edd9becd2bde93439720e469dae52980d75b5f

---
+++ moodle-auto-login/moodle-auto-login.user.js
@@ -0,0 +1,15 @@
+/*! For license information please see moodle-auto-login.user.js.LICENSE.txt */
+(()=>{"use strict";
+// ==UserScript==
+// @name       Moodle Auto Login
+// @version    2.0.0
+// @author     lusc
+// @match      *://moodle.*/*
+// @match      *://moodle*.*/*
+// @grant      GM_setValue
+// @grant      GM_getValue
+// @run-at     document-start
+// @license    MIT
+// @updateURL  https://github.com/melusc/moodle_userscripts/raw/userscript-out/moodle-auto-login/moodle-auto-login.user.js
+// ==/UserScript==
+"https:"!==location.protocol&&(location.protocol="https:");const e=()=>{GM_setValue("last",Date.now())},t=(t,o,n)=>{t.addEventListener("submit",(()=>{GM_setValue("username",o.value),GM_setValue("password",n.value),e()}))};var o;location.pathname.startsWith("/login/index.php")&&(o=()=>{const o=document.querySelector("#username"),n=document.querySelector("#password"),a=document.querySelector("#login"),s=GM_getValue("password"),r=GM_getValue("username");if(!o||!n||!a)return;if(!s||!r)return void t(a,o,n);o.value=r;const i=Date.now()-GM_getValue("last"),l=document.querySelector("#loginerrormessage"),u=l?.textContent;if(!Number.isFinite(i)||i>1e4||u&&/session has timed out/i.test(u))return n.value=s,e(),void a.submit();u&&/invalid login/i.test(u)||console.log("[%cMoodle Auto Login%c] Last attempt was too recently, waiting %ds to prevent infinite loop.","color: #0074d9","",(1e4-i)/1e3),t(a,o,n)},"interactive"!==document.readyState&&"complete"!==document.readyState?document.addEventListener("DOMContentLoaded",o,{once:!0}):o())})();
---
+++ no-force-download/no-force-download.user.js
@@ -0,0 +1,14 @@
+/*! For license information please see no-force-download.user.js.LICENSE.txt */
+(()=>{"use strict";var e;
+// ==UserScript==
+// @name       No Force Download
+// @match      *://moodle.*/*
+// @match      *://moodle*.*/*
+// @grant      none
+// @version    2.0.0
+// @author     lusc
+// @license    MIT
+// @updateURL  https://github.com/melusc/moodle_userscripts/raw/userscript-out/no-force-download/no-force-download.user.js
+// @run-at     document-start
+// ==/UserScript==
+e=()=>{const e=document.querySelectorAll('a[href*="?"][href*="forcedownload"]');for(const t of e)if(t.search){const e=new URLSearchParams(t.search);e.delete("forcedownload"),t.search=`?${e.toString()}`}},"interactive"!==document.readyState&&"complete"!==document.readyState?document.addEventListener("DOMContentLoaded",e,{once:!0}):e()})();
