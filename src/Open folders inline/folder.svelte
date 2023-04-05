0ebc445a35cc1082fdbfeeccc836235b95566ecc
Luca <61409445+melusc@users.noreply.github.com>
2023-04-05 17:53:57 +0200

    Rewrite Open Folders Inline with Svelte (#606)
    * Rewrite Open Folders Inline with Svelte
    
    * Bump version
    
    * Rename file from .tsx to .ts

---
+++ src/Open folders inline/folder.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+	import {onMount} from 'svelte';
+	import RefreshIcon from './refresh-icon.svelte';
+	import {
+		getSanitizedContents,
+		type SanitizedContentFile,
+	} from './page-content.js';
+	import FolderRoot from './folder-root.svelte';
+
+	export let folderId: string;
+	export let sectionId: string;
+	export let anchor: HTMLElement;
+
+	let contents: undefined | SanitizedContentFile[];
+	let isHidden = false;
+
+	onMount(() => {
+		void fetchContents(true);
+	});
+
+	let prevAnchor: HTMLElement | undefined = undefined;
+	$: {
+		if (prevAnchor !== anchor) {
+			prevAnchor?.removeEventListener('click', anchorClickHandler);
+		}
+		prevAnchor = anchor;
+		anchor.addEventListener('click', anchorClickHandler);
+	}
+
+	async function fetchContents(allowCached: boolean) {
+		contents = undefined;
+		const result = await getSanitizedContents(sectionId, folderId, allowCached);
+
+		if (result) {
+			contents = result;
+		}
+	}
+
+	function anchorClickHandler(event: MouseEvent): void {
+		if (event.ctrlKey) {
+			return;
+		}
+
+		event.preventDefault();
+		event.stopImmediatePropagation();
+
+		isHidden = !isHidden;
+	}
+</script>
+
+{#if !isHidden}
+	{#if !contents}
+		<div class="folder-loading">Loading</div>
+	{:else if contents.length === 0}
+		<div class="folder-empty">The folder was empty</div>
+	{:else}
+		<FolderRoot directoryDepth={0} isParent {contents} />
+	{/if}
+
+	<RefreshIcon
+		target={anchor}
+		on:click={() => {
+			console.log('click');
+			void fetchContents(false);
+		}}
+	/>
+{/if}
