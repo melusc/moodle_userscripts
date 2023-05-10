f84b18ab7aafeba07b7ddc60f6529f7542b863b1
Luca <61409445+melusc@users.noreply.github.com>
2023-05-10 16:04:57 +0200

    Open Folders Inline: Only request folders and only in relevant section

--- src/Open folders inline/page-content.ts
+++ src/Open folders inline/page-content.ts
@@ -4,6 +4,7 @@ import type {
 	FolderModule,
 } from '../shared/moodle-functions-v3/course-content.d.js';
 import {getCourseContent} from '../shared/moodle-functions-v3/get-course-content.js';
+import {memoise} from '../shared/moodle-functions-v3/memoise.js';
 import {Moodle} from '../shared/moodle-functions-v3/moodle.js';
 import {popupLogin} from '../shared/moodle-functions-v3/popup-login-svelte.js';
 
@@ -19,7 +20,7 @@ Moodle.extend(popupLogin).extend(getCourseContent);
 const moodle = new Moodle();
 
 const getPageContent = async (
-	useCache: boolean,
+	sectionId: string,
 ): Promise<false | CourseContent[]> => {
 	const searchParameters = new URLSearchParams(location.search);
 	const courseId = searchParameters.get('id');
@@ -31,73 +32,82 @@ const getPageContent = async (
 	}
 
 	try {
-		return await moodle.getCourseContent(courseId, useCache);
+		return await moodle.fetch<CourseContent[]>('core_course_get_contents', {
+			courseid: courseId,
+			// Limit to the section that has the folder
+			'options[0][name]': 'sectionid',
+			'options[0][value]': sectionId,
+			// Only folders
+			'options[1][name]': 'modname',
+			'options[1][value]': 'folder',
+		});
 	} catch {
 		await moodle.popupLogin('Open folders inline', true);
-		return moodle.getCourseContent(courseId, useCache);
+		return getPageContent(sectionId);
 	}
 };
 
 const sanitizedFilePath = (path: string): string[] =>
 	path.trim().split(/\/+/).filter(Boolean);
 
-export const getSanitizedContents = async (
-	sectionId: string,
-	folderId: string,
-	allowCached: boolean,
-): Promise<false | SanitizedContentFile[]> => {
-	const pageContentJSON = await getPageContent(allowCached);
+export const getSanitizedContents = memoise(
+	async (
+		sectionId: string,
+		folderId: string,
+	): Promise<false | SanitizedContentFile[]> => {
+		const pageContentJSON = await getPageContent(sectionId);
 
-	if (pageContentJSON === false) {
-		return false;
-	}
-
-	const sectionObject = pageContentJSON.find(
-		({id}) => id === Number(sectionId),
-	);
-
-	if (!sectionObject) {
-		console.error('Could not find sectionObject.');
-
-		return false;
-	}
-
-	const {modules} = sectionObject;
-
-	const folderObject = modules.find(
-		({id, modname}) => modname === 'folder' && id === Number(folderId),
-	) as FolderModule | undefined;
-
-	if (!folderObject) {
-		console.error('Could not find folderObject.');
-
-		return false;
-	}
-
-	const {contents} = folderObject;
-	const sanitizedContents: SanitizedContentFile[] = [];
-
-	for (const item of contents) {
-		if (item.type === 'file') {
-			const {filepath, mimetype} = item;
-
-			const fileUrl = moodle.resolveUrl(item.fileurl);
-			fileUrl.pathname = fileUrl.pathname.replace(/^\/webservice/, '');
-
-			const imgPath = new URL(fileUrl.href);
-
-			if (!mimetype?.startsWith('image')) {
-				imgPath.searchParams.set('preview', '1');
-			}
-
-			sanitizedContents.push({
-				...item,
-				filePath: sanitizedFilePath(filepath),
-				imgPath: getImageURL(mimetype, imgPath.href),
-				fileUrl: fileUrl.href,
-			});
+		if (pageContentJSON === false) {
+			return false;
 		}
-	}
 
-	return sanitizedContents;
-};
+		const sectionObject = pageContentJSON.find(
+			({id}) => id === Number(sectionId),
+		);
+
+		if (!sectionObject) {
+			console.error('Could not find sectionObject.');
+
+			return false;
+		}
+
+		const {modules} = sectionObject;
+
+		const folderObject = modules.find(
+			({id, modname}) => modname === 'folder' && id === Number(folderId),
+		) as FolderModule | undefined;
+
+		if (!folderObject) {
+			console.error('Could not find folderObject.');
+
+			return false;
+		}
+
+		const {contents} = folderObject;
+		const sanitizedContents: SanitizedContentFile[] = [];
+
+		for (const item of contents) {
+			if (item.type === 'file') {
+				const {filepath, mimetype} = item;
+
+				const fileUrl = moodle.resolveUrl(item.fileurl);
+				fileUrl.pathname = fileUrl.pathname.replace(/^\/webservice/, '');
+
+				const imgPath = new URL(fileUrl.href);
+
+				if (!mimetype?.startsWith('image')) {
+					imgPath.searchParams.set('preview', '1');
+				}
+
+				sanitizedContents.push({
+					...item,
+					filePath: sanitizedFilePath(filepath),
+					imgPath: getImageURL(mimetype, imgPath.href),
+					fileUrl: fileUrl.href,
+				});
+			}
+		}
+
+		return sanitizedContents;
+	},
+);
